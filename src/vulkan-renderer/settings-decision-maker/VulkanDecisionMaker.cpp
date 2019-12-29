cc7512daaa5e4fdad76176129102887f7541e2d0
IAmNotHanni <cmdfreak@web.de>
2019-12-29 15:10:47 +0100

    Introducing VulkanDecisionMaker class.
    The class VulkanDecisionMaker is responsible for making decisions which affect the use of the features which are available on the system.
    TODO: Implement!

---
+++ src/vulkan-renderer/settings-decision-maker/VulkanDecisionMaker.cpp
@@ -0,0 +1,19 @@
+#include "VulkanDecisionMaker.hpp"
+
+
+namespace inexor {
+namespace vulkan_renderer {
+
+
+	VulkanDecisionMaker::VulkanDecisionMaker()
+	{
+	}
+
+
+	VulkanDecisionMaker::~VulkanDecisionMaker()
+	{
+	}
+
+
+};
+};
