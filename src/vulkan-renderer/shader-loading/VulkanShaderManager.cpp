ae2dd8fbb9fdd1f5d44adc0cbc393fa4807eb421
IAmNotHanni <cmdfreak@web.de>
2020-01-02 17:36:08 +0100

    New error-handling strategy for VulkanShaderManager.

--- src/vulkan-renderer/shader-loading/VulkanShaderManager.cpp
+++ src/vulkan-renderer/shader-loading/VulkanShaderManager.cpp
@@ -15,7 +15,7 @@ namespace vulkan_renderer {
 	}
 
 
-	void VulkanShaderManager::create_shader_module(const VkDevice& vulkan_device, const std::vector<char>& SPIRV_shader_bytes, VkShaderModule* shader_module)
+	VkResult VulkanShaderManager::create_shader_module(const VkDevice& vulkan_device, const std::vector<char>& SPIRV_shader_bytes, VkShaderModule* shader_module)
 	{
 		VkShaderModuleCreateInfo shader_create_info = {};
 		
@@ -25,25 +25,25 @@ namespace vulkan_renderer {
 		shader_create_info.codeSize = SPIRV_shader_bytes.size();
 		shader_create_info.pCode = reinterpret_cast<const uint32_t*>(SPIRV_shader_bytes.data());
 
-		VkResult result = vkCreateShaderModule(vulkan_device, &shader_create_info, nullptr, shader_module);
-		vulkan_error_check(result);
+		return vkCreateShaderModule(vulkan_device, &shader_create_info, nullptr, shader_module);
 	}
 
 
-	void VulkanShaderManager::create_shader_module_from_file(const VkDevice& vulkan_device, const std::string& SPIRV_file_name, VkShaderModule* shader_module)
+	VkResult VulkanShaderManager::create_shader_module_from_file(const VkDevice& vulkan_device, const std::string& SPIRV_file_name, VkShaderModule* shader_module)
 	{
 		cout << "Creating shader from file: " << SPIRV_file_name.c_str() << endl;
 
+		// Load the SPIR-V shader file.
 		VulkanShader vulkan_shader;
-
 		vulkan_shader.load_file(SPIRV_file_name);
 		
 		if(0 == vulkan_shader.file_size)
 		{
 			std::string error_message = "Error: SPIR-V shader file " + SPIRV_file_name + "is empty!";
-			display_error_message(error_message);		}
+			display_error_message(error_message);
+		}
 
-		create_shader_module(vulkan_device, vulkan_shader.file_data, shader_module);
+		return create_shader_module(vulkan_device, vulkan_shader.file_data, shader_module);
 	}
 
 
--- src/vulkan-renderer/shader-loading/VulkanShaderManager.hpp
+++ src/vulkan-renderer/shader-loading/VulkanShaderManager.hpp
@@ -5,14 +5,13 @@
 #include <iostream>
 using namespace std;
 
-#include <vulkan/vulkan.h>
-
 #include "../error-handling/VulkanErrorHandling.hpp"
 #include "VulkanShader.hpp"
 
 
 namespace inexor {
 namespace vulkan_renderer {
+
 
 	// A class for managing SPIR-V shaders.
 	class VulkanShaderManager
@@ -23,16 +22,22 @@ namespace vulkan_renderer {
 			
 			~VulkanShaderManager();
 
+			
 		protected:
 
-			// 
-			void create_shader_module(const VkDevice& vulkan_device, const std::vector<char>& SPIRV_shader_bytes, VkShaderModule* shader_module);
+			// Creates a shader module.
+			// @param vulkan_device The Vulkan device handle.
+			// @param SPIRV_shader_bytes The binary data of the shader.
+			// @param shader_module The shader module.
+			VkResult create_shader_module(const VkDevice& vulkan_device, const std::vector<char>& SPIRV_shader_bytes, VkShaderModule* shader_module);
 
-			// 
-			void create_shader_module_from_file(const VkDevice& vulkan_device, const std::string& SPIRV_file_name, VkShaderModule* shader_module);
+			// Creates a shader module from a SPIR-V shader file.
+			// @param vulkan_device The Vulkan device handle.
+			// @param SPIRV_file_name The name of the SPIR-V shader file.
+			// @param shader_module The shader module.
+			VkResult create_shader_module_from_file(const VkDevice& vulkan_device, const std::string& SPIRV_file_name, VkShaderModule* shader_module);
 
 	};
-
 
 };
 };
