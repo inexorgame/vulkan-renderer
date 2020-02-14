625d7e426a672d286b487276cf2b4faaece78fe2
IAmNotHanni <cmdfreak@web.de>
2020-02-14 20:04:38 +0100

    Improved GPU selection mechanism in VulkanSettingsDecisionMaker.

--- src/vulkan-renderer/settings-decision-maker/VulkanSettingsDecisionMaker.cpp
+++ src/vulkan-renderer/settings-decision-maker/VulkanSettingsDecisionMaker.cpp
@@ -17,6 +17,8 @@ namespace vulkan_renderer {
 	
 	uint32_t VulkanSettingsDecisionMaker::decide_how_many_images_in_swapchain_to_use(const VkPhysicalDevice& graphics_card, const VkSurfaceKHR& surface)
 	{
+		cout << "Deciding automatically how many images in swapchain to use." << endl;
+
 		uint32_t number_of_images_in_swapchain = 0;
 
 		VkSurfaceCapabilitiesKHR surface_capabilities = {};
@@ -24,10 +26,12 @@ namespace vulkan_renderer {
 		VkResult result = vkGetPhysicalDeviceSurfaceCapabilitiesKHR(graphics_card, surface, &surface_capabilities);
 		vulkan_error_check(result);
 
+		// TODO: Refactor! How many images do we actually need? Is triple buffering the best option?
+
 		// Determine how many images in swapchain to use.
 		number_of_images_in_swapchain = surface_capabilities.minImageCount + 1;
 
-		// If the maximum number of images available in swapchain is greater than our current number, chose it
+		// If the maximum number of images available in swapchain is greater than our current number, chose it.
 		if((surface_capabilities.maxImageCount > 0) && (surface_capabilities.maxImageCount < number_of_images_in_swapchain))
 		{
 			number_of_images_in_swapchain = surface_capabilities.maxImageCount;
@@ -39,29 +43,31 @@ namespace vulkan_renderer {
 
 	VkResult VulkanSettingsDecisionMaker::decide_which_surface_color_format_in_swapchain_to_use(const VkPhysicalDevice& graphics_card, const VkSurfaceKHR& surface, VkFormat& color_format, VkColorSpaceKHR& color_space)
 	{
-		uint32_t number_of_surface_formats = 0;
+		cout << "Deciding automatically which surface color format in swapchain to use." << endl;
+
+		uint32_t number_of_available_surface_formats = 0;
 
 		// First check how many surface formats are available.
-		VkResult result = vkGetPhysicalDeviceSurfaceFormatsKHR(graphics_card, surface, &number_of_surface_formats, nullptr);
+		VkResult result = vkGetPhysicalDeviceSurfaceFormatsKHR(graphics_card, surface, &number_of_available_surface_formats, nullptr);
 		if(VK_SUCCESS != result) return result;
 
-		if(!(number_of_surface_formats > 0))
+		if(0 == number_of_available_surface_formats)
 		{
 			std::string error_message = "Error: No surface formats could be found by fpGetPhysicalDeviceSurfaceFormatsKHR!";
 			display_error_message(error_message);
 		}
 		
 		// Preallocate memory for available surface formats.
-		std::vector<VkSurfaceFormatKHR> available_surface_formats(number_of_surface_formats);
+		std::vector<VkSurfaceFormatKHR> available_surface_formats(number_of_available_surface_formats);
 		
 		// Get information about all surface formats available.
-		result = vkGetPhysicalDeviceSurfaceFormatsKHR(graphics_card, surface, &number_of_surface_formats, available_surface_formats.data());
+		result = vkGetPhysicalDeviceSurfaceFormatsKHR(graphics_card, surface, &number_of_available_surface_formats, available_surface_formats.data());
 		vulkan_error_check(result);
 
 		
 		// If the surface format list only includes one entry with VK_FORMAT_UNDEFINED,
 		// there is no preferred format, so we assume VK_FORMAT_B8G8R8A8_UNORM.
-		if(1 == number_of_surface_formats && VK_FORMAT_UNDEFINED == available_surface_formats[0].format)
+		if(1 == number_of_available_surface_formats && VK_FORMAT_UNDEFINED == available_surface_formats[0].format)
 		{
 			color_format = VK_FORMAT_B8G8R8A8_UNORM;
 			color_space = available_surface_formats[0].colorSpace;
@@ -97,8 +103,85 @@ namespace vulkan_renderer {
 		return VK_SUCCESS;
 	}
 
+
+	VkBool32 VulkanSettingsDecisionMaker::decide_if_graphics_card_is_suitable(const VkPhysicalDevice& graphics_card)
+	{
+		// The properties of the graphics card.
+		VkPhysicalDeviceProperties graphics_card_properties;
+
+		// The features of the graphics card.
+		VkPhysicalDeviceFeatures graphics_card_features;
+
+		// Get the information about that graphics card's properties.
+		vkGetPhysicalDeviceProperties(graphics_card, &graphics_card_properties);
+		
+		// Get the information about the graphics card's features.
+		vkGetPhysicalDeviceFeatures(graphics_card, &graphics_card_features);
+
+
+		cout << "Checking suitability of graphics card " << graphics_card_properties.deviceName << "." << endl;
+
+		// This mechanism checks if a graphics card is suitable for the application's purpose.
+		
+		// In contrast to the original plan, we will not discriminate graphics cards which
+		// are not VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU, as this would deny many players
+		// to run Inexor on their machines!
+
+		// Check if geometry shaders are supported.
+		if(!graphics_card_features.geometryShader)
+		{
+			cout << "This device is not suitable since it does not support geometry shaders." << endl;
+			return false;
+		}
+
+
+		// Check if swapchain is supported.
+		bool swapchain_is_supported = false;
+
+		uint32_t number_of_available_device_extensions = 0;
+
+		VkResult result = vkEnumerateDeviceExtensionProperties(graphics_card, nullptr, &number_of_available_device_extensions, nullptr);
+		vulkan_error_check(result);
+
+		if(0 == number_of_available_device_extensions)
+		{
+			display_error_message("Error: No Vulkan device extensions available!");
+
+			// Since there are no device extensions available at all, the desired one is not supported either.
+			swapchain_is_supported = false;
+		}
+		else
+		{
+			// Preallocate memory for device extensions.
+			std::vector<VkExtensionProperties> device_extensions(number_of_available_device_extensions);
+			result = vkEnumerateDeviceExtensionProperties(graphics_card, nullptr, &number_of_available_device_extensions, device_extensions.data());
+			vulkan_error_check(result);
+
+			// Loop through all available device extensions and search for the requested one.
+			for(const VkExtensionProperties& device_extension : device_extensions)
+			{
+				if(0 == strcmp(device_extension.extensionName, VK_KHR_SWAPCHAIN_EXTENSION_NAME))
+				{
+					swapchain_is_supported = true;
+				}
+			}
+		}
+
+		if(!swapchain_is_supported)
+		{
+			cout << "This device is not suitable since it does not support swap chains." << endl;
+			return false;
+		}
+
+		// TODO: Check if the selected device supports queue families for graphics bits and presentation!
+		
+		// Add more suitability checks here if desired.
+
+		return true;
+	}
+
 	
-	VkPhysicalDevice VulkanSettingsDecisionMaker::decide_which_graphics_card_to_use(const VkInstance& vulkan_instance, const int& preferred_graphics_card_index)
+	VkPhysicalDevice VulkanSettingsDecisionMaker::decide_which_graphics_card_to_use(const VkInstance& vulkan_instance, const uint32_t& preferred_graphics_card_index)
 	{
 		cout << "Deciding automatically which graphics card to use." << endl;
 		
@@ -108,10 +191,10 @@ namespace vulkan_renderer {
 		VkResult result = vkEnumeratePhysicalDevices(vulkan_instance, &number_of_available_graphics_cards, nullptr);
 		vulkan_error_check(result);
 
-		if(number_of_available_graphics_cards <= 0)
+		if(0 == number_of_available_graphics_cards)
 		{
 			display_error_message("Error: Could not find any graphics cards!");
-			// TODO: Shutdown Vulkan and application!
+			// TODO: Return std::nullopt.
 		}
 		
 		cout << "There are " << number_of_available_graphics_cards << " graphics cards available." << endl;
@@ -126,46 +209,130 @@ namespace vulkan_renderer {
 		// If there is only 1 graphics card available, we don't have a choice and must use that one.
 		if(1 == available_graphics_cards.size())
 		{
-			cout << "Because there is only 1 graphics card, we don't have a choice and must use that one." << endl;
+			// Did the user specify a preferred GPU by command line argument?
+			if(UINT32_MAX != preferred_graphics_card_index)
+			{
+				if(0 != preferred_graphics_card_index)
+				{
+					// A little hint message just to be sure.
+					cout << "Ignoring command line argument -GPU " << preferred_graphics_card_index << " because there is only one GPU to chose from." << endl;
+				}
+				if(!(preferred_graphics_card_index >= 0 && preferred_graphics_card_index < available_graphics_cards.size()))
+				{
+					// Haha.
+					cout << "Array index would have been invalid anyways!" << endl;
+				}
+			}
 
-			// We are done: use the first graphics card!
+			cout << "Because there is only 1 graphics card, we don't have a choice and must use that one." << endl;
 			return available_graphics_cards[0];
 		}
 
-		// The user passed a command line parameter -gpu_index <NUMBER> which determines the graphics card array index to use.
-		if(-1 != preferred_graphics_card_index)
+		// The user passed a command line parameter to the application which specifies the array index of the GPU to use.
+		if(UINT32_MAX != preferred_graphics_card_index)
 		{
 			// Check if this array index is valid!
 			if(preferred_graphics_card_index >= 0 && preferred_graphics_card_index < available_graphics_cards.size())
 			{
-				cout << "Command line parameter -gpu_index specified. Using graphics card index " << preferred_graphics_card_index << "." << endl;
+				cout << "Command line parameter for desired GPU specified. Checking graphics card with index " << preferred_graphics_card_index << "." << endl;
 	
-				// TODO: Check if the graphics card selected by the user meets all the criteria we need!
+				bool selected_graphics_card_is_suitable = false;
+				
+				// Check if the graphics card selected by the user meets all the criteria we need!
+				selected_graphics_card_is_suitable = decide_if_graphics_card_is_suitable(available_graphics_cards[preferred_graphics_card_index]);
 
-				// We are done: Use the graphics card which was specified by the user's command line argument.
-				return available_graphics_cards[preferred_graphics_card_index];
+				if(selected_graphics_card_is_suitable)
+				{
+					// We are done: Use the graphics card which was specified by the user's command line argument.
+					return available_graphics_cards[preferred_graphics_card_index];
+				}
+				else
+				{
+					cout << "Error: The preferred graphics card with index " << preferred_graphics_card_index << " is not suitable for this application!" << endl;
+					cout << "The array index is valid, but this graphics card does not fulfill all requirements!" << endl;
+
+					// We are NOT done!
+					// Try to select the best graphics card automatically!
+				}
 			}
 			else
 			{
 				// No, this array index for available_graphics_cards is invalid!
 				cout << "Error: invalid command line argument! Graphics card array index " << preferred_graphics_card_index << " is invalid!" << endl;
 
-				// We are NOT done: Select the best graphics card automatically!
+				// We are NOT done!
+				// Try to select the best graphics card automatically!
 			}
 		}
+		else
+		{
+			cout << "No command line argument for preferred graphics card given." << endl;
+		}
 
-		cout << "No command line argument for preferred graphics card given. Detecting best graphics card automatically." << endl;
+		cout << "Detecting best graphics card automatically." << endl;
+		cout << "Phase 1: Sort out all graphics cards which are unsuitable." << endl;
 
-		uint32_t available_graphics_cards_array_index = 0;
+		uint32_t available_graphics_cards_array_index = UINT32_MAX;
 		
+		// The suitable graphics cards (by array index).
+		std::vector<std::size_t> suitable_graphics_cards;
+
+		// Loop through all available graphics cards and sort out the unsuitable ones.
+		for(std::size_t i=0; i<number_of_available_graphics_cards; i++)
+		{
+			if(decide_if_graphics_card_is_suitable(available_graphics_cards[i]))
+			{
+				cout << "Adding graphics card index " << i << " to the list of suitable graphics cards." << endl;
+
+				// Add this graphics card to the list of suitable graphics cards.
+				suitable_graphics_cards.push_back(i);
+			}
+			else
+			{
+				cout << "Sorting out graphics card index " << i << " because it is unsuitable for this application's purpose!" << endl;
+			}
+		}
 		
+		// How many graphics cards have been sorted out?
+		auto how_many_graphics_card_disqualified = number_of_available_graphics_cards - suitable_graphics_cards.size();
 
-		// TODO: Implement!
+		if(how_many_graphics_card_disqualified > 0)
+		{
+			cout << how_many_graphics_card_disqualified << " have been disqualified because they are unsuitable for the application's purpose!" << endl;
+		}
 
-		// TODO: Check if the selected device supports swapchains.
-		// TODO: Check if the selected device supports queue families for graphics bits and presentation!
-		// TODO: Prefere VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU!
+		// We could not find any suitable graphics card!
+		if(0 == suitable_graphics_cards.size())
+		{
+			cout << "Error: Could not find suitable graphics card automatically." << endl;
+			//return std::nullopt;
+		}
 
+		// Only 1 graphics card is suitable, let's choose that one.
+		if(1 == suitable_graphics_cards.size())
+		{
+			available_graphics_cards_array_index = suitable_graphics_cards[0];
+			return available_graphics_cards[available_graphics_cards_array_index];
+		}
+
+		// We have more than one suitable graphics card.
+		// The situation is more complex.
+
+		// There is at least one graphics card that is suitable at least.
+		// This means available_graphics_cards_array_index can no longer be UINT32_MAX from now on!1
+
+		cout << "Phase 2: We have multiple suitable graphics card." << endl;
+		cout << "Starting to rank them by score." << endl;
+
+		
+		// ----------------------------------------
+		// TODO: Implement score ranking mechanism!
+		// ----------------------------------------
+
+
+		// For now, let's just use the first index of the suitable graphics cards.
+		// This might be not the best choice after all, but it simplifies the situation for now.
+		available_graphics_cards_array_index = suitable_graphics_cards[0];
 
 
 
--- src/vulkan-renderer/settings-decision-maker/VulkanSettingsDecisionMaker.hpp
+++ src/vulkan-renderer/settings-decision-maker/VulkanSettingsDecisionMaker.hpp
@@ -13,13 +13,12 @@ namespace inexor {
 namespace vulkan_renderer {
 
 
-	// @class VulkanSettingsDecisionMaker
-	// @brief This class makes automatic decisions on e.g. the following topics:
-	// - Which graphics card will be used if more than 1 is available?
-	// - Which surface color format should be used?
-	// - Which graphics card's queue families should be used?
-	// - Which presentation modes should be used?
-	// @note The methods also verify that every selected option is available on the current system.
+	/// @class VulkanSettingsDecisionMaker
+	/// @brief This class makes automatic decisions which are relevant to setting up Vulkan:
+	/// - Which graphics card will be used if more than 1 is available?
+	/// - Which surface color format should be used?
+	/// - Which graphics card's queue families should be used?
+	/// - Which presentation modes should be used?
 	class VulkanSettingsDecisionMaker
 	{
 		public:
@@ -30,55 +29,73 @@ namespace vulkan_renderer {
 
 
 		protected:
+			
+			
+			/// @brief Automatically decides if a graphics cardis suitable for this application's purpose!
+			/// @note Add more checks to the validation mechanism if neccesary, e.h. check for geometry shader support.
+			/// @param graphics_card The graphics card which will be checked for suitability.
+			/// @return True if the graphics card is suitable, false otherwise.
+			VkBool32 decide_if_graphics_card_is_suitable(const VkPhysicalDevice& graphics_card);
 
-			// Check which graphics card will be used in case multiple are available.
-			// If there is only one graphics card available, we must use it without a choice.
-			// The user can specify which graphics card will be used manually by passing a command line argument.
-			// A graphics card is only suitable if it supports the following list of features:
-			// ---------------------------------------------------------------------------------------------------------
-			// 1) One queue family that supports both graphics and present or 2 queue families for graphics and present.
-			// 2) Present mode VK_PRESENT_MODE_MAILBOX_KHR or VK_PRESENT_MODE_FIFO_KHR.
-			// 3) Support of double buffering, even better triple buffering.
-			// TODO: Add more criteria if neccesary!
-			VkPhysicalDevice decide_which_graphics_card_to_use(const VkInstance& vulkan_instance, const int& preferred_graphics_card_index = -1);
+
+			// TODO: Turn into std::optional values!
+
+			/// @brief Automatically select the best graphics card in case multiple are available.
+			/// @note If there is only one graphics card available, we can't choose and must use it obviously.
+			/// @note The user can manually specify which graphics card will be used by passing a command line argument.
+			/// @note A graphics card is only suitable if it supports the following list of features:
+			/// - One queue family that supports both graphics and present or 2 queue families for graphics and present.
+			/// - Present mode VK_PRESENT_MODE_MAILBOX_KHR or VK_PRESENT_MODE_FIFO_KHR.
+			/// - Support of double buffering, even better triple buffering.
+			/// 
+			/// @param vulkan_instance A pointer to the Vulkan instance handle.
+			/// @param preferred_graphics_card_index The preferred graphics card (by array index).
+			/// @return The physical device which was chosen to be the best one.
+			VkPhysicalDevice decide_which_graphics_card_to_use(const VkInstance& vulkan_instance, const uint32_t& preferred_graphics_card_index = UINT32_MAX);
 
 			
-			// 
-			// @param graphics_card The selected graphics card.
-			// @param surface The selected (window) surface.
+			/// @brief Automatically decides how many images will be used in the swap chain.
+			/// @param graphics_card The selected graphics card.
+			/// @param surface The selected (window) surface.
+			/// @return The number of images that will be used in swap chain.
 			uint32_t decide_how_many_images_in_swapchain_to_use(const VkPhysicalDevice& graphics_card, const VkSurfaceKHR& surface);
 			
+
 			// 
 			VkResult decide_which_surface_color_format_in_swapchain_to_use(const VkPhysicalDevice& graphics_card, const VkSurfaceKHR& surface, VkFormat& color_format, VkColorSpaceKHR& color_space);
+
 
 			// 
 			void decide_which_surface_color_format_in_swapchain_to_use(const VkPhysicalDevice& graphics_card, const VkSurfaceKHR& surface, uint32_t& image_width, uint32_t& image_height);
 
+
 			// 
 			void decide_width_and_height_of_swapchain_extent(const VkPhysicalDevice& graphics_card, const VkSurfaceKHR& surface, uint32_t& window_width, uint32_t& window_height, VkExtent2D& swapchain_extent);
 
-			//uint32_t decide_which_graphics_card_queue_family_index_to_use(const VkPhysicalDevice& graphics_card);
-
-			//uint32_t decide_which_graphics_card_queue_family_index_to_use();
-
-			//uint32_t decide_size_of_swap_chain_images();
-
-			//void decide_which_pre_transformation_to_use();
-
-			//decide_which_image_color_space_to_use();
 			
+			// TODO
+			//uint32_t decide_which_graphics_card_queue_family_index_to_use(const VkPhysicalDevice& graphics_card);
+			//uint32_t decide_which_graphics_card_queue_family_index_to_use();
+			//uint32_t decide_size_of_swap_chain_images();
+			//void decide_which_pre_transformation_to_use();
+			//decide_which_image_color_space_to_use();
 			//decide_which_image_sharing_mode_to_use();
 
 
-			// Find the transform, relative to the presentation engine's natural orientation, applied to the image content prior to presentation.
+			/// @brief Find the transform, relative to the presentation engine's natural orientation, applied to the image content prior to presentation.
+			/// @brief 
+			/// @brief 
+			/// @return 
 			VkSurfaceTransformFlagsKHR decide_which_image_transformation_to_use(const VkPhysicalDevice& graphics_card, const VkSurfaceKHR& surface);
 
 
-			// @brief Decides which presentation mode the presentation engine will be using.
-			// We can only use presentation modes that are available in the current system.
-			// The preferred presentation mode is VK_PRESENT_MODE_MAILBOX_KHR.
-			// @warning Just checking whether swap extension is supported is not enough because presentation support is a queue family property!
-			// A physical device may support swap chains, but that doesn't mean that all its queue families also support it.
+			/// @brief Automatically decides which presentation mode the presentation engine will be using.
+			/// @note We can only use presentation modes that are available in the current system. The preferred presentation mode is VK_PRESENT_MODE_MAILBOX_KHR.
+			/// @warning Just checking whether swap extension is supported is not enough because presentation support is a queue family property!
+			/// A physical device may support swap chains, but that doesn't mean that all its queue families also support it.
+			/// @param graphics_card The selected graphics card.
+			/// @param surface The selected (window) surface.
+			/// @return The presentation mode which will be used by the presentation engine.
 			VkPresentModeKHR decide_which_presentation_mode_to_use(const VkPhysicalDevice& graphics_card, const VkSurfaceKHR& surface);
 
 	};
