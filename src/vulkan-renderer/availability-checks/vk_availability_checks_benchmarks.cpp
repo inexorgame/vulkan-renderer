4fdf634660248d9bfa8664fd506d6dca37e94739
IAmNotHanni <johannesschneider@outlook.com>
2020-04-06 00:06:23 +0200

    Removed old benchmarks and tests.
    Closes https://github.com/inexorgame/vulkan-renderer/issues/41.
    Tests and benchmarks are empty now!

--- src/vulkan-renderer/availability-checks/vk_availability_checks_benchmarks.cpp
+++ src/vulkan-renderer/availability-checks/vk_availability_checks_benchmarks.cpp
@@ -1,26 +1,3 @@
 #include <benchmark/benchmark.h>
 
-#include "vk_availability_checks.hpp"
-using namespace inexor::vulkan_renderer::availability_checks;
-
-
-static void BM_is_instance_layer_available(benchmark::State& state)
-{
-    for(auto _ : state)
-    {
-        is_instance_layer_available("VK_LAYER_LUNARG_standard_validation");
-    }
-}
-
-BENCHMARK(BM_is_instance_layer_available);
-
-
-static void BM_is_instance_extension_available(benchmark::State& state)
-{
-    for(auto _ : state)
-    {
-        is_instance_extension_available("VK_EXT_debug_utils");
-    }
-}
-
-BENCHMARK(BM_is_instance_extension_available);
+// TODO: Add benchmarks!
--- src/vulkan-renderer/availability-checks/vk_availability_checks_tests.cpp
+++ src/vulkan-renderer/availability-checks/vk_availability_checks_tests.cpp
@@ -1,33 +1,4 @@
 #include <gtest/gtest.h>
 #include <spdlog/spdlog.h>
 
-#include "vk_availability_checks.hpp"
-
-
-TEST(availability_checks, instance_layer_available)
-{
-	using namespace inexor::vulkan_renderer::availability_checks;
-
-	// Check if standard validation layer is available.
-	bool retval = is_instance_layer_available("VK_LAYER_LUNARG_standard_validation");
-
-	EXPECT_EQ(retval, true);
-}
-
-
-TEST(availability_checks, instance_extension_available)
-{
-	using namespace inexor::vulkan_renderer::availability_checks;
-
-	bool retval[3];
-
-	retval[0] = is_instance_extension_available("VK_EXT_debug_utils");
-	retval[1] = is_instance_extension_available("VK_EXT_debug_marker");
-	retval[2] = is_instance_extension_available("VK_EXT_debug_report");
-
-	// The test succeeded if one of these extensions could be found.
-	// If none could be found, there might be something wrong with the method!
-	bool result = retval[0] || retval[1] || retval[2];
-
-	EXPECT_EQ(result, true);
-}
+// TODO: Add tests!
