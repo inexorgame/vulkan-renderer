6abf2ff10fd74d519aa36f1dbcd40f67fa97492a
IAmNotHanni <johannesschneider@outlook.com>
2020-04-12 13:47:52 +0200

    Fix Linux build (5).

--- src/vulkan-renderer/keyboard/keyboard_input.hpp
+++ src/vulkan-renderer/keyboard/keyboard_input.hpp
@@ -1,41 +1,43 @@
-#pragma once
+﻿#pragma once
 
 #include <cassert>
 
 #include <spdlog/spdlog.h>
 
-#include <glfw/glfw3.h>
+#include <GLFW/glfw3.h>
 
 
-namespace inexor {
-namespace vulkan_renderer {
-
-
-	/// This needs some serious redesign!
-	/// Should we use polling instead of callbacks?
-	/// This would resolve the issue of using static void callback functions instead of class methods!
-	/// Also, we might could run this in a separate thread.
-
-
-	/// @class InexorKeyboardInputHandler.
-	class InexorKeyboardInputHandler
+namespace inexor
+{
+	namespace vulkan_renderer
 	{
-		public:
+
+
+		/// This needs some serious redesign!
+		/// Should we use polling instead of callbacks?
+		/// This would resolve the issue of using static void callback functions instead of class methods!
+		/// Also, we might could run this in a separate thread.
+
+
+		/// @class InexorKeyboardInputHandler.
+		class InexorKeyboardInputHandler
+		{
+			public:
 
 			InexorKeyboardInputHandler() = default;
-			
+
 			~InexorKeyboardInputHandler() = default;
 
 
-		protected:
+			protected:
 
 			/// @brief Initialises keyboard input handler.
 			/// @param window [in] The glfw window.
 			/// @param keyboard_input_callback [in] The glfw keyboard input callback.
 			void initialise(GLFWwindow* window, GLFWkeyfun keyboard_input_callback);
 
+		};
+
+
 	};
-
-
-};
 };
