29335652b708865291af52aba4c0e2092e479c6d
Johannes <johannesschneider@outlook.com>
2020-04-20 16:41:02 +0200

    [bezier] Added simple bezier curve implementation based on glm.
    Closes https://github.com/inexorgame/vulkan-renderer/issues/30.

---
+++ src/vulkan-renderer/bezier-curve/bezier_curve.cpp
@@ -0,0 +1,133 @@
+#include "bezier_curve.h"
+
+namespace inexor {
+namespace vulkan_renderer {
+
+uint32_t BezierCurve::binomial_coefficient(uint32_t n, const uint32_t k) {
+    uint32_t r = 1;
+    if (k > n)
+        return 0;
+
+    for (uint32_t d = 1; d <= k; d++) {
+        r *= n--;
+        r /= d;
+    }
+
+    return r;
+}
+
+float BezierCurve::bernstein_polynomial(uint32_t n, uint32_t k, const float curve_precision, const float coordinate_value) {
+    return binomial_coefficient(n, k) * static_cast<float>(pow(curve_precision, k)) * static_cast<float>(pow(1 - curve_precision, n - k)) * coordinate_value;
+}
+
+void BezierCurve::clear_output() { std::vector<BezierOutputPoint> output_points; }
+
+void BezierCurve::clear_input() { std::vector<BezierInputPoint> input_points; }
+
+void BezierCurve::clear() {
+    clear_input();
+    clear_output();
+}
+
+bool BezierCurve::is_curve_generated() { return curve_generated; }
+
+std::vector<BezierOutputPoint> BezierCurve::get_output_points() {
+    assert(curve_generated);
+    assert(output_points.size() > 0);
+    return output_points;
+}
+
+void BezierCurve::add_input_point(const BezierInputPoint &input_point) {
+    assert(!curve_generated);
+    input_points.push_back(input_point);
+}
+
+void BezierCurve::add_input_point(const glm::vec3 &position, const float weight) {
+    assert(!curve_generated);
+    assert(weight > 0.0f);
+
+    BezierInputPoint input_point;
+    input_point.pos = position;
+    input_point.weight = weight;
+
+    input_points.push_back(input_point);
+}
+
+BezierOutputPoint BezierCurve::calculate_point_on_curve(const float curve_precision) {
+    BezierOutputPoint temp_output;
+
+    uint32_t n = static_cast<uint32_t>(input_points.size() - 1);
+
+    // Calculate the coordinates of the output points of the bezier curve.
+    for (std::size_t i = 0; i < input_points.size(); i++) {
+        auto current_point = input_points[i];
+
+        uint32_t index = static_cast<uint32_t>(i);
+
+        // Compute bezier curve coordinates using bernstein polynomials.
+        temp_output.pos.x += bernstein_polynomial(n, index, curve_precision, current_point.pos.x);
+        temp_output.pos.y += bernstein_polynomial(n, index, curve_precision, current_point.pos.y);
+        temp_output.pos.z += bernstein_polynomial(n, index, curve_precision, current_point.pos.z);
+    }
+
+    // Calculate the derivatives of bezier curves.
+    // https://www.rose-hulman.edu/~finn/CCLI/Notes/day13.pdf
+
+    // An easier way would be to take the vector to the next point on the curve and calculate the difference.
+    // This would lead to a direction vector with satisfying precision. But this would implicate that the
+    // precision of the derivation depends on the curve precision!
+    // With this technique, we can have precise derivations even if we have a precision of only 10.0f units!
+    for (std::size_t i = 0; i < n; i++) {
+        auto current_point = input_points[i];
+        auto next_point = input_points[i + 1];
+
+        uint32_t index = static_cast<uint32_t>(i);
+
+        // Compute bezier curve point's first derivative.
+        temp_output.tangent.x += bernstein_polynomial(n - 1, index, curve_precision, current_point.pos.x);
+        temp_output.tangent.y += bernstein_polynomial(n - 1, index, curve_precision, current_point.pos.y);
+        temp_output.tangent.z += bernstein_polynomial(n - 1, index, curve_precision, current_point.pos.z);
+    }
+
+    // Subtract point position from tangent vector so the tangent vector is relative.
+    temp_output.tangent -= temp_output.pos;
+
+    // Calculate a relative normal vector.
+    // Please note that there is an infinite amount of normal vectors from vector a to b.
+
+    // TODO: Glm uses int as length() ?
+    float length = static_cast<float>(temp_output.tangent.length());
+    temp_output.normal = glm::vec3(-temp_output.tangent.y / length, temp_output.tangent.x / length, 0);
+
+    // Do not normalize tangent vectors before you have copied the normal vector! They will be incorrect!
+
+    // Normalize the vectors: divide them by length so their length is 1.
+    // You could rescale the vectors to change the length to your wishes.
+    glm::normalize(temp_output.normal);
+    glm::normalize(temp_output.tangent);
+
+    // Store the output.
+    return temp_output;
+}
+
+void BezierCurve::calculate_bezier_curve(const float curve_precision) {
+    // We need at least 2 input points!
+    assert(input_points.size() > 2);
+
+    if (curve_generated) {
+        clear();
+        curve_generated = false;
+    }
+
+    float curve_precision_interval = 1.0f / curve_precision;
+
+    for (float position_on_curve = 0.0f; position_on_curve <= 1.0f; position_on_curve += curve_precision_interval) {
+        BezierOutputPoint temp_output = calculate_point_on_curve(position_on_curve);
+        output_points.push_back(temp_output);
+    }
+
+    curve_generated = true;
+}
+
+}; // namespace vulkan_renderer
+}; // namespace inexor
---
+++ src/vulkan-renderer/bezier-curve/bezier_curve.h
@@ -0,0 +1,99 @@
+#pragma once
+
+// TODO: Implement a curve manager.
+
+/// INTRODUCTION
+/// A bezier curve (named after french mathematician PIERRE ETIENNE BEZIER) is a parametric curve
+/// whose only purpose is to look soft and smooth. Bezier curves are all about elegance!
+/// Those curves can be used to represent the path of a everything (imagin a camera which is moving along a path for example).
+///
+/// Bezier curves are fast, flexible, beautiful and easy to compute. You just pass a bunch of parameter points to
+/// your code and the final curve will be computed. Because every complex curve can be represented with a
+/// chain of smaller curves, it is recommended to create a chain of curves. Bezier curves are essential
+/// in the field of computer graphics and image processing. They can also be used for approximation, interpolation and more.
+///
+/// COMPUTING
+/// There are two ways to generate a bezier curves from a group of [n] points.
+/// You can either write a code that uses recursion to solve the problem or use Bernstein polynomials.
+/// This engine uses Bernstein polynomial, because we want to avoid the recursion in de-casteljau algorithm.
+///
+/// Pierre Etienne BEZIER       (September 1, 1910 - November 25, 1999), French mathematician and engineer at RENAULT.
+/// Paul de CASTELJAU           (November 19, 1930), French mathematician and physicist and engineer ar Citroen.
+/// Sergei Natanovich BERNSTEIN (March 5, 1880 - October 26, 1968), Russian mathematician.
+/// Charles HERMITE             (December 24, 1822 - January 14, 1901), French mathematician.
+
+/// http://pomax.github.io/bezierinfo/
+/// http://en.wikipedia.org/wiki/B%C3%A9zier_curve
+/// http://mathworld.wolfram.com/BezierCurve.html
+/// http://theagsc.com/community/tutorials/so-whats-the-big-deal-with-horizontal-vertical-bezier-handles-anyway#comment-1351842776
+/// http://learn.scannerlicker.net/2014/04/16/bezier-curves-and-type-design-a-tutorial/
+/// https://geom.ivd.kit.edu/downloads/pubs/pub-boehm-prautzsch_2002_preview.pdf
+/// https://www.clear.rice.edu/comp360/lectures/BezSubd.pdf
+
+#include <glm/glm.hpp>
+
+#include <cmath>
+#include <vector>
+
+namespace inexor {
+namespace vulkan_renderer {
+
+/// @brief Those are the points that we pass into the bezier curve generator.
+/// Every bezier curve will be generated from a list of BezierInputPoint.
+/// Every input point can have a custom weight coefficient.
+struct BezierInputPoint {
+    glm::vec3 pos = glm::vec3(0.0f, 0.0f, 0.0f);
+
+    float weight = 1.0f;
+};
+
+/// @brief Those are the points which will be generated from the bezier curve generator.
+/// How many BezierOutputPoint points will be generated depends on the required precision.
+/// The higher the requested precision, the more points will be
+struct BezierOutputPoint : public BezierInputPoint {
+    glm::vec3 normal = glm::vec3(0.0f, 0.0f, 0.0f);
+
+    glm::vec3 tangent = glm::vec3(0.0f, 0.0f, 0.0f);
+};
+
+/// @brief This struct bundles describes everything about the bezier curve.
+/// It contains both the input points and the generated output points.
+class BezierCurve {
+    bool curve_generated;
+
+    float curve_precision;
+
+    std::vector<BezierInputPoint> input_points;
+
+    std::vector<BezierOutputPoint> output_points;
+
+    uint32_t BezierCurve::binomial_coefficient(uint32_t n, const uint32_t k);
+
+    float bernstein_polynomial(uint32_t n, uint32_t k, const float curve_precision, const float coordinate_value);
+
+    BezierOutputPoint calculate_point_on_curve(const float curve_precision);
+
+public:
+    BezierCurve() = default;
+
+    ~BezierCurve() = default;
+
+    void add_input_point(const BezierInputPoint &input_point);
+
+    void add_input_point(const glm::vec3 &position, const float weight = 1.0f);
+
+    void calculate_bezier_curve(const float curve_precision);
+
+    std::vector<BezierOutputPoint> get_output_points();
+
+    void clear_output();
+
+    void clear_input();
+
+    void clear();
+
+    bool is_curve_generated();
+};
+
+}; // namespace vulkan_renderer
+}; // namespace inexor
