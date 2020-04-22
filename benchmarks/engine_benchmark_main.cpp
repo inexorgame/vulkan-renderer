3361b9125532b7b496b5d7e5fb66e3d5f15f50f7
Iceflower <iceflower@gmx.de>
2020-04-22 15:40:58 +0200

    [code] Remove unnecessary colons

--- benchmarks/engine_benchmark_main.cpp
+++ benchmarks/engine_benchmark_main.cpp
@@ -3,11 +3,11 @@
 #include <iostream>
 
 int main(int argc, char **argv) {
-    ::benchmark::Initialize(&argc, argv);
-    if (::benchmark::ReportUnrecognizedArguments(argc, argv)) {
+    benchmark::Initialize(&argc, argv);
+    if (benchmark::ReportUnrecognizedArguments(argc, argv)) {
         return 1;
     }
-    ::benchmark::RunSpecifiedBenchmarks();
+    benchmark::RunSpecifiedBenchmarks();
 
     std::cin.get();
 }
--- tests/unit_tests_main.cpp
+++ tests/unit_tests_main.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 
 int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
+    testing::InitGoogleTest(&argc, argv);
     RUN_ALL_TESTS();
     std::cin.get();
 }
