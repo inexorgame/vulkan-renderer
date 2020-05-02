984f82207ebc8119b20e508de22df8316b0e3c4a
Johannes <johannesschneider@outlook.com>
2020-05-02 17:41:11 +0200

    [docs] Added doxygen documentation comments for shader.
    Remove unnecessary vulkan memory allocator header include.

--- include/inexor/vulkan-renderer/shader.hpp
+++ include/inexor/vulkan-renderer/shader.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <vma/vk_mem_alloc.h>
 #include <vulkan/vulkan.h>
 
 #include <string>
@@ -18,16 +17,28 @@ private:
     VkShaderModule shader_module;
 
 public:
-    /// @brief Creates a shader from a SPIR-V memory block.
+    /// @brief Creates a shader from memory.
+    /// @param device [in] The Vulkan device which will be used to create the shader module.
+    /// @param type [in] The shader type (vertex shader, fragment shader, tesselation shader..).
+    /// @param name [in] The internal name of the shader module.
+    /// @param code [in] The SPIR-V shader code.
+    /// @param entry_point [in] The entry point of the shader code, in most cases just "main".
     Shader(VkDevice device, VkShaderStageFlagBits type, const std::string &name, const std::vector<char> &code, const std::string &entry_point = "main");
 
     /// @brief Creates a shader from a SPIR-V file.
+    /// @param device [in] The Vulkan device which will be used to create the shader module.
+    /// @param type [in] The shader type (vertex shader, fragment shader, tesselation shader..).
+    /// @param name [in] The internal name of the shader module.
+    /// @param file_name [in] The name of the SPIR-V shader file.
+    /// @param entry_point [in] The entry point of the shader code, in most cases just "main".
     Shader(VkDevice device, VkShaderStageFlagBits type, const std::string &name, const std::string &file_name, const std::string &entry_point = "main");
 
+    /// Delete the copy constructor so Shaders are move-only objects.
     Shader(const Shader &) = delete;
-    Shader(Shader && shader) noexcept;
+    Shader(Shader &&shader) noexcept;
     ~Shader();
 
+    /// Delete the copy assignment operator so Shaders are move-only objects.
     Shader &operator=(const Shader &) = delete;
     Shader &operator=(Shader &&) noexcept = default;
 
