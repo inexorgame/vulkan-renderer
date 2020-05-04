21f469c4ceb65d7601fa62ea5fa7715be7e04b98
Johannes <johannesschneider@outlook.com>
2020-05-04 20:53:19 +0200

    [refactoring] Replaced SingleTimeCommandBufferRecorder with OnceCommandBuffer.

---
+++ include/inexor/vulkan-renderer/once_command_buffer.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <vulkan/vulkan.h>
+
+#include <spdlog/spdlog.h>
+
+#include <cassert>
+#include <stdexcept>
+
+namespace inexor::vulkan_renderer {
+
+/// @brief A OnceCommandBuffer is a command buffer which is being used only once.
+class OnceCommandBuffer {
+private:
+    VkDevice device = VK_NULL_HANDLE;
+    VkCommandPool command_pool = VK_NULL_HANDLE;
+    VkQueue queue = VK_NULL_HANDLE;
+    bool recording_started = false;
+    VkCommandBuffer command_buffer = VK_NULL_HANDLE;
+
+public:
+    // Delete the copy constructor so once command buffers are move-only objects.
+    OnceCommandBuffer(const OnceCommandBuffer &) = delete;
+    OnceCommandBuffer(OnceCommandBuffer &&other) noexcept;
+
+    // Delete the copy assignment operator so shaders are move-only objects.
+    OnceCommandBuffer &operator=(const OnceCommandBuffer &) = delete;
+    OnceCommandBuffer &operator=(OnceCommandBuffer &&) noexcept = default;
+
+    /// @brief Creates a new commandbuffer which is being called only once.
+    /// @param device [in] The Vulkan device.
+    /// @param data_transfer_queue [in] The data transfer queue.
+    OnceCommandBuffer(const VkDevice &device, const VkQueue &data_transfer_queue);
+
+    ~OnceCommandBuffer();
+
+    void start_recording();
+
+    void end_recording_and_submit_command();
+
+    const VkCommandBuffer get_command_buffer() const {
+        return command_buffer;
+    }
+};
+
+} // namespace inexor::vulkan_renderer
