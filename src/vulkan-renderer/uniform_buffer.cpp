53fa44eabe6f0a60b0aa12418543e830bc0ca393
Johannes <johannesschneider@outlook.com>
2020-05-04 21:22:21 +0200

    [refactoring] Fix build: pass on memory usage flags in correct order.

--- src/vulkan-renderer/uniform_buffer.cpp
+++ src/vulkan-renderer/uniform_buffer.cpp
@@ -7,7 +7,7 @@ UniformBuffer::UniformBuffer(UniformBuffer &&other) noexcept
     : GPUMemoryBuffer(std::move(other)), descriptor_buffer_info(std::move(other.descriptor_buffer_info)), descriptor_set(std::move(other.descriptor_set)) {}
 
 UniformBuffer::UniformBuffer(const VkDevice &device, const VmaAllocator &vma_allocator, std::string &name, const VkDeviceSize &buffer_size)
-    : GPUMemoryBuffer(device, vma_allocator, name, buffer_size, VMA_MEMORY_USAGE_CPU_TO_GPU, VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT) {}
+    : GPUMemoryBuffer(device, vma_allocator, name, buffer_size, VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT, VMA_MEMORY_USAGE_CPU_TO_GPU) {}
 
 void UniformBuffer::update(void *data, const std::size_t size) {
     assert(allocation_info.pMappedData);
