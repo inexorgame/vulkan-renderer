0460283a155d6651ab52d1b626d510a35466e441
Johannes <johannesschneider@outlook.com>
2020-05-04 20:56:05 +0200

    [refactoring] Added new texture class.

--- include/inexor/vulkan-renderer/texture.hpp
+++ include/inexor/vulkan-renderer/texture.hpp
@@ -1,64 +1,119 @@
 #pragma once
 
 #include "inexor/vulkan-renderer/gpu_memory_buffer.hpp"
+#include "inexor/vulkan-renderer/once_command_buffer.hpp"
+#include "inexor/vulkan-renderer/staging_buffer.hpp"
 
 #include <vulkan/vulkan.h>
 
+#include <cassert>
 #include <string>
+
+#include <nlohmann/json.hpp>
+#include <spdlog/spdlog.h>
+#include <vma/vma_usage.h>
+#include <vulkan/vulkan.h>
+#define TINYGLTF_NO_INCLUDE_JSON
+#include <tiny_gltf/tiny_gltf.h>
 
 namespace inexor::vulkan_renderer {
 
+// TODO: 3D textures and cube maps.
+// TODO: Scan asset directory automatically.
+// TODO: Create multiple textures from file and submit them in 1 command buffer for performance reasons.
+
 class Texture {
-public:
-    Texture() = default;
-
-    ~Texture() = default;
-
+private:
     std::string name = "";
-
     std::string file_name = "";
+    int texture_width = 0;
+    int texture_height = 0;
+    int texture_channels = 0;
+    int mip_levels = 0;
 
-    VkImageViewCreateInfo view_create_info = {};
+    VkDevice device = VK_NULL_HANDLE;
+    VkPhysicalDevice graphics_card = VK_NULL_HANDLE;
+    VkQueue data_transfer_queue = VK_NULL_HANDLE;
 
-    VkImageCreateInfo image_create_info = {};
+    VmaAllocator vma_allocator = VK_NULL_HANDLE;
+    VmaAllocation allocation;
+    VmaAllocationInfo allocation_info;
 
-    VmaAllocation allocation = VK_NULL_HANDLE;
+    VkImage image = VK_NULL_HANDLE;
+    VkImageView image_view = VK_NULL_HANDLE;
+    VkSampler sampler = VK_NULL_HANDLE;
+    VkFormat texture_image_format = VK_FORMAT_R8G8B8A8_UNORM;
 
-    VmaAllocationInfo allocation_info = {};
+    OnceCommandBuffer copy_command_buffer;
 
-    VkBufferCreateInfo create_info = {};
+    ///
+    void transition_image_layout(VkImage &image, VkFormat format, VkImageLayout old_layout, VkImageLayout new_layout);
 
-    VmaAllocationCreateInfo allocation_create_info = {};
+    ///
+    void create_texture_image_view();
 
-    VkImage image = {};
+    ///
+    void create_texture_sampler();
 
-    VkFormat format = {};
+public:
+    /// Delete the copy constructor so textures are move-only objects.
+    Texture(const Texture &) = delete;
+    Texture(Texture &&other) noexcept;
 
-    VkImageView image_view = {};
+    /// Delete the copy assignment operator so shaders are move-only objects.
+    Texture &operator=(const Texture &) = delete;
+    Texture &operator=(Texture &&) noexcept = default;
 
-    VkImageLayout image_layout = {};
+    /// @brief Creates a texture from a file.
+    /// @param device [in] The Vulkan device from which the texture will be created.
+    /// @param graphics_card [in] The graphics card.
+    /// @param command_pool [in] The command pool.
+    /// @param command_buffer [in] The command buffer.
+    /// @param vma_allocator [in] The Vulkan Memory Allocator library handle.
+    /// @param file_name [in] The file name of the texture.
+    /// @param name [in] The internal memory allocation name of the texture.
+    /// @param data_transfer_queue_family_index [in] The queue family index which will be used for uploading texture data to GPU memory.
+    /// @param data_transfer_queue [in] The Vulkan data transfer queue.
+    Texture(const VkDevice &device, const VkPhysicalDevice &graphics_card, const VkCommandPool &command_pool, const VkCommandBuffer &command_buffer,
+            const VmaAllocator &vma_allocator, const std::string &file_name, const std::string &name, const std::uint32_t &data_transfer_queue_family_index,
+            const VkQueue &data_transfer_queue);
 
-    VkDescriptorImageInfo descriptor = {};
+    /// @brief Creates a texture from memory.
+    /// @param device [in] The Vulkan device from which the texture will be created.
+    /// @param graphics_card [in] The graphics card.
+    /// @param command_pool [in] The command pool.
+    /// @param command_buffer [in] The command buffer.
+    /// @param vma_allocator [in] The Vulkan Memory Allocator library handle.
+    /// @param texture_data [in] The texture data.
+    /// @param texture_size [in] The size of the texture.
+    /// @param name [in] The internal memory allocation name of the texture.
+    /// @param data_transfer_queue_family_index [in] The queue family index which will be used for uploading texture data to GPU memory.
+    /// @param data_transfer_queue [in] The Vulkan data transfer queue.
+    Texture(const VkDevice &device, const VkPhysicalDevice &graphics_card, const VkCommandPool &command_pool, const VkCommandBuffer &command_buffer,
+            const VmaAllocator &vma_allocator, void *texture_data, const std::size_t texture_size, std::string &name,
+            const std::uint32_t &data_transfer_queue_family_index, const VkQueue &data_transfer_queue);
 
-    VkSampler sampler = {};
+    ~Texture();
 
-    std::uint32_t layer_count = 0;
+    const std::string &get_name() const {
+        return name;
+    }
 
-    std::uint32_t mip_levels = 0;
+    const std::string &get_file_name() const {
+        return file_name;
+    }
 
-    std::uint32_t width = 0;
+    const VkImage get_image() const {
+        return image;
+    }
 
-    std::uint32_t height = 0;
+    const VkImageView get_image_view() const {
+        return image_view;
+    }
 
-    // TODO: Refactoring: Remove methods! only let VulkanTextureManager change data.
-
-    /// @brief Destroys a texture.
-    /// @param device [in] The Vulkan device.
-    /// @param vma_allocator [in] The Vulkan memory allocator handle.
-    void destroy_texture(const VkDevice &device, const VmaAllocator &vma_allocator);
-
-    /// @brief Updates the texture sampler's descriptor.
-    void update_descriptor();
+    const VkSampler get_sampler() const {
+        return sampler;
+    }
 };
 
 } // namespace inexor::vulkan_renderer
