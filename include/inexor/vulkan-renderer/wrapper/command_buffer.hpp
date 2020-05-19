797be316bb8cf1963055981b984736203c50efc2
Johannes <johannesschneider@outlook.com>
2020-05-19 22:29:21 +0200

    [refactoring] Added RAII wrapper for VkCommandBuffer.

---
+++ include/inexor/vulkan-renderer/wrapper/command_buffer.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <spdlog/spdlog.h>
+#include <vulkan/vulkan_core.h>
+
+namespace inexor::vulkan_renderer::wrapper {
+class CommandBuffer {
+private:
+    VkCommandBuffer command_buffer;
+
+public:
+    /// Delete the copy constructor so command buffers are move-only objects.
+    CommandBuffer(const CommandBuffer &) = delete;
+    CommandBuffer(CommandBuffer &&other) noexcept;
+
+    /// Delete the copy assignment operator so command buffers are move-only objects.
+    CommandBuffer &operator=(const CommandBuffer &) = delete;
+    CommandBuffer &operator=(CommandBuffer &&other) noexcept = default;
+
+    CommandBuffer(const VkDevice device, const VkCommandPool command_pool);
+
+    /// @note We don't need to destroy the command buffer because it will be destroyed along with it's associated command pool automatically.
+    ~CommandBuffer() = default;
+
+    [[nodiscard]] VkCommandBuffer get() const {
+        return command_buffer;
+    }
+};
+} // namespace inexor::vulkan_renderer::wrapper
---
+++ src/vulkan-renderer/wrapper/command_buffer.cpp
@@ -0,0 +1,18 @@
+#include "inexor/vulkan-renderer/wrapper/command_buffer.hpp"
+
+namespace inexor::vulkan_renderer::wrapper {
+CommandBuffer::CommandBuffer(CommandBuffer &&other) noexcept : command_buffer(std::exchange(other.command_buffer, nullptr)) {}
+
+CommandBuffer::CommandBuffer(const VkDevice device, const VkCommandPool command_pool) {
+    VkCommandBufferAllocateInfo alloc_info = {};
+    alloc_info.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+    alloc_info.commandBufferCount = 1;
+    alloc_info.commandPool = command_pool;
+    alloc_info.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+
+    if (vkAllocateCommandBuffers(device, &alloc_info, &command_buffer) != VK_SUCCESS) {
+        throw std::runtime_error("Error: vkAllocateCommandBuffers failed for once command buffer!");
+    }
+}
+
+} // namespace inexor::vulkan_renderer::wrapper
