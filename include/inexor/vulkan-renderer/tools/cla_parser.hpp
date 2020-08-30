7f86bf27009cbe49ad8caf82b5e0fe4900066ce1
Owen Smith <ojman101@gmail.com>
2020-08-31 00:27:08 +0100

    [tools] Cleanup command line parser
    * Cleanup TODOs
    * Improve doc comments
    * Remove unnecessary newlines
    * Reorder argument templates to match the ones in docs

--- include/inexor/vulkan-renderer/tools/cla_parser.hpp
+++ include/inexor/vulkan-renderer/tools/cla_parser.hpp
@@ -9,14 +9,14 @@
 
 namespace inexor::vulkan_renderer::tools {
 
-/// @brief Template class for creating arguments
+/// @brief Template class for creating arguments.
 class CommandLineArgumentTemplate {
     const std::string m_argument;
     const bool m_takes_values;
 
 public:
-    /// @param argument The argument to be passed on the command line (e.g --vsync)
-    /// @param takes_args Whether this argument can take values (e.g --gpu 1)
+    /// @param argument The argument to be passed on the command line (e.g. --vsync)
+    /// @param takes_args Whether this argument can take values (e.g. --gpu 1)
     /// @note Only arguments that take zero or one values are supported
     CommandLineArgumentTemplate(std::string argument, bool takes_args)
         : m_argument(std::move(argument)), m_takes_values(takes_args) {}
@@ -41,62 +41,62 @@ public:
 };
 
 /// @brief A simple command line argument parser.
-/// @note Only supports arguments with zero or one values (e.g --vsync or --gpu 1)
-/// @note Only supports long arguments (e.g --<arg>)
-/// @todo Support equals arguments and arguments with multiple values (e.g --gpus=1,2,3)
-/// @todo Support short arguments with stacking (e.g -abc)
+/// @note Only supports arguments with zero or one values (e.g. --vsync or --gpu 1)
+/// @note Only supports long arguments (e.g. --<arg>)
+// TODO: Support equals arguments and arguments with multiple values (e.g. --gpus=1,2,3).
+// TODO: Support short arguments, including stacking (e.g. -abc).
 class CommandLineArgumentParser {
-    /// @todo Allow runtime addition of accepted parameters
+    // TODO: Allow runtime addition of argument templates.
     const std::vector<CommandLineArgumentTemplate> m_accepted_args = {
-        // Defines which GPU to use (by array index).
+        // Specifies which GPU to use (by array index).
         {"--gpu", true},
 
-        // Defines if we will print stats about graphics cards.
-        {"--no-stats", false},
-
-        // Use vertical synchronisation.
-        {"--vsync", false},
-
-        // Use RenderDoc layer.
-        {"--renderdoc", false},
-
-        // Disable Khronos validation layer.
-        {"--no-validation", false},
-
-        // Do not use distinct data transfer queue, use graphics queue.
+        // Disables the use of the special data transfer queue (forces use of the graphics queue).
         {"--no-separate-data-queue", false},
 
-        // Disable debug markers (even if -renderdoc is specified)
-        {"--no-vk-debug-markers", false}};
+        // Disables GPU info printing.
+        {"--no-stats", false},
+
+        // Disables the Khronos vulkan validation layer.
+        {"--no-validation", false},
+
+        // Disables vulkan debug markers (even if --renderdoc is specified).
+        {"--no-vk-debug-markers", false},
+
+        // Enables the RenderDoc debug layer.
+        {"--renderdoc", false},
+
+        // Enables vertical synchronisation (limits FPS to monitor refresh rate).
+        {"--vsync", false}};
 
     std::unordered_map<std::string, CommandLineArgumentValue> m_parsed_arguments;
 
     std::optional<CommandLineArgumentTemplate> make_arg_template(const std::string &argument_name) const;
 
 public:
-    /// @brief Parses the command line arguments
+    /// @brief Parses the command line arguments.
+    /// @param argc The number of arguments passed in the command line
+    /// @param argv An array of strings containing `argc` elements
     void parse_args(int argc, char **argv);
 
+    /// @brief Attempts to retrieve the argument `name` as type `T`.
+    /// @param name The whole name of the argument (e.g. --renderdoc)
+    /// @return `std::nullopt` if argument `name` wasn't specified by the user at all, `true` if the argument was
+    ///          specified but doesn't take any values and the first value of the argument lexically casted to type `T`.
     template <typename T>
     std::optional<T> arg(const std::string &name) const {
         auto arg_template = make_arg_template(name);
-        if (!arg_template) {
-            return std::nullopt;
-        }
-
         auto it = m_parsed_arguments.find(name);
-        if (it == m_parsed_arguments.end()) {
+        if (!arg_template || it == m_parsed_arguments.end()) {
             return std::nullopt;
         }
-
         if (!arg_template->takes_values()) {
             return true;
         }
-
         return it->second.as<T>();
     }
 
-    /// @brief Returns the number of command line arguments.
+    /// @brief Returns the number of parsed command line arguments.
     [[nodiscard]] std::size_t parsed_arg_count() const {
         return m_parsed_arguments.size();
     }
--- src/vulkan-renderer/tools/cla_parser.cpp
+++ src/vulkan-renderer/tools/cla_parser.cpp
@@ -16,11 +16,9 @@ bool CommandLineArgumentValue::as() const {
     if (m_value == "false") {
         return false;
     }
-
     if (m_value == "true") {
         return true;
     }
-
     return static_cast<bool>(as<int>());
 }
 
@@ -31,41 +29,42 @@ std::uint32_t CommandLineArgumentValue::as() const {
 
 std::optional<CommandLineArgumentTemplate>
 CommandLineArgumentParser::make_arg_template(const std::string &argument_name) const {
-    // clang-format off
-    auto it = std::find_if(m_accepted_args.begin(), m_accepted_args.end(), [&](const auto &accepted_arg) {
-        return argument_name == accepted_arg.argument();
-    });
-    // clang-format on
-
+    auto it = std::find_if(m_accepted_args.begin(), m_accepted_args.end(),
+                           [&](const auto &accepted_arg) { return argument_name == accepted_arg.argument(); });
     if (it == m_accepted_args.end()) {
         return std::nullopt;
     }
-
     return *it;
 }
 
+// TODO: Take in an std::vector<std::string> of arguments? This would avoid any hard to debug out of bounds errors (with
+//       argc being bigger than the number of elements actually in argv).
 void CommandLineArgumentParser::parse_args(int argc, char **argv) {
     for (int i = 1; i < argc; i++) {
         // NOLINTNEXTLINE
         std::string arg_name(argv[i]);
-        auto arg_template = make_arg_template(arg_name);
 
+        // If a user enters an unrecognized argument, just warn them about it for now.
+        auto arg_template = make_arg_template(arg_name);
         if (!arg_template) {
             spdlog::warn("Unknown command line argument {}!", arg_name);
             continue;
         }
 
+        // Insert a dummy value if the argument doesn't take any.
         if (!arg_template->takes_values()) {
             m_parsed_arguments.insert(std::make_pair(arg_name, ""));
             continue;
         }
 
-        if (i + 1 >= argc) {
+        // We have to fail if the user doesn't specify a value for an argument expecting one (since it would mess up the
+        // rest of parsing).
+        if (i++ >= argc) {
             throw std::runtime_error("No value specified for argument " + arg_name);
         }
 
         // NOLINTNEXTLINE
-        std::string arg_value(argv[++i]);
+        std::string arg_value(argv[i]);
         m_parsed_arguments.insert(std::make_pair(arg_name, arg_value));
     }
 }
