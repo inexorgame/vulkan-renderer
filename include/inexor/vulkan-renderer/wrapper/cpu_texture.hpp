4137517fd1d2f75df20fb4a380ee031928b63bf0
Johannes <johannesschneider@outlook.com>
2020-09-19 10:30:57 +0200

    [code] Fix msvc code analysis warning C26451
    Signed-off-by: Johannes <johannesschneider@outlook.com>

--- include/inexor/vulkan-renderer/wrapper/cpu_texture.hpp
+++ include/inexor/vulkan-renderer/wrapper/cpu_texture.hpp
@@ -65,7 +65,8 @@ public:
 
     [[nodiscard]] std::size_t data_size() const {
         // TODO: We will need to update this once we fully support mip levels.
-        return m_texture_width * m_texture_height * m_texture_channels;
+        return static_cast<std::size_t>(m_texture_width) * static_cast<std::size_t>(m_texture_height) *
+               static_cast<std::size_t>(m_texture_channels);
     }
 };
 
