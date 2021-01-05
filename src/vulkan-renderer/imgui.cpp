84ea6df11932e47f017f31b6a4b6363a26a1df6c
Iceflower <iceflower@gmx.de>
2021-01-05 23:50:56 +0100

    [renderer] Fix imgui initial layout

--- src/vulkan-renderer/imgui.cpp
+++ src/vulkan-renderer/imgui.cpp
@@ -1,4 +1,4 @@
-﻿#include "inexor/vulkan-renderer/imgui.hpp"
+#include "inexor/vulkan-renderer/imgui.hpp"
 
 #include "inexor/vulkan-renderer/exceptions/vk_exception.hpp"
 #include "inexor/vulkan-renderer/wrapper/cpu_texture.hpp"
@@ -135,7 +135,7 @@ ImGUIOverlay::ImGUIOverlay(const wrapper::Device &device, const wrapper::Swapcha
     attachments[0].storeOp = VK_ATTACHMENT_STORE_OP_STORE;
     attachments[0].stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
     attachments[0].stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
-    attachments[0].initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+    attachments[0].initialLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
     attachments[0].finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
 
     VkAttachmentReference color_ref = {};
