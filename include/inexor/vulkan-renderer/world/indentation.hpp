d1caf322b1b82693a55a320eb8fed3922b867db7
Iceflower <iceflower@gmx.de>
2021-01-07 23:01:00 +0100

    [octree] Add rotation function

--- include/inexor/vulkan-renderer/world/indentation.hpp
+++ include/inexor/vulkan-renderer/world/indentation.hpp
@@ -39,6 +39,8 @@ public:
     void indent_start(std::int8_t steps) noexcept;
     /// Positive steps into the direction of start.
     void indent_end(std::int8_t steps) noexcept;
+    /// Mirror the indentation, such that the distance from 0 to start and distance from end to max switches
+    void mirror() noexcept;
 
     [[nodiscard]] std::uint8_t uid() const;
 };
