5fd88e50f93151fc36a5980d1ed6ac88b32af9f9
Iceflower <iceflower@gmx.de>
2021-01-12 22:51:02 +0100

    [*] Move GPU info to vk tools

--- include/inexor/vulkan-renderer/exceptions/vk_exception.hpp
+++ include/inexor/vulkan-renderer/exceptions/vk_exception.hpp
@@ -8,22 +8,11 @@ namespace inexor::vulkan_renderer::exceptions {
 
 /// @brief
 class VulkanException final : public Exception {
-private:
-    ///@brief Return a VkResult's description text.
-    /// @note This function can be used for both VkResult error and success values.
-    /// @param result The VkResult return value which will be turned into a string.
-    [[nodiscard]] static std::string get_vkresult_description(const VkResult result);
-
-    /// @brief Turn a VkResult into a string.
-    /// @note This function can be used for both VkResult error and success values.
-    /// @param result The VkResult return value which will be turned into a string.
-    [[nodiscard]] static std::string get_vkresult_string(const VkResult result);
-
 public:
     /// @brief Default constructor.
     /// @param message The exception message.
     /// @param result The VkResult value of the Vulkan API call which failed.
-    VulkanException(const std::string message, const VkResult result);
+    VulkanException(const std::string &message, VkResult result);
 };
 
 } // namespace inexor::vulkan_renderer::exceptions
