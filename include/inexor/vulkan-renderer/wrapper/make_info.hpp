bd78d6bc827ffe6dc096c33c6d3f37857d4f963a
Iceflower <iceflower@gmx.de>
2021-03-27 22:43:18 +0100

    [doc] Fix documentation building
    Fixed doxygen usage
    Workaround for exhale bug

--- include/inexor/vulkan-renderer/wrapper/make_info.hpp
+++ include/inexor/vulkan-renderer/wrapper/make_info.hpp
@@ -3,7 +3,9 @@
 namespace inexor::vulkan_renderer::wrapper {
 
 /// @brief A small helper function that return vulkan create infos with sType already set
-/// @example auto render_pass_ci = make_info<VkRenderPassCreateInfo>();
+/// @code{.cpp}
+/// auto render_pass_ci = make_info<VkRenderPassCreateInfo>();
+/// @endcode
 /// @note Also zeros the returned struct
 template <typename T>
 [[nodiscard]] T make_info();
