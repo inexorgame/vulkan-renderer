c45d3bf93e031f27e05415d5ff207eab4fc966a1
movabo <mvb1996@gmail.com>
2021-04-10 02:03:26 +0200

    [octree] Add (face) neighbor-method and save the index of the cube in the parents array in `Cube::m_child_in_parent`

---
+++ tests/world/cube.cpp
@@ -0,0 +1,30 @@
+#include <inexor/vulkan-renderer/world/cube.hpp>
+
+#include <gtest/gtest.h>
+
+namespace {
+using namespace inexor::vulkan_renderer::world;
+
+TEST(Cube, neighbor) {
+    std::shared_ptr<Cube> root = std::make_shared<Cube>(2.0f, glm::vec3{0, -1, -1});
+    root->set_type(Cube::Type::OCTANT);
+
+    for (const auto &child : root->children()) {
+        child->set_type(Cube::Type::OCTANT);
+
+        for (const auto &grandchild : child->children()) {
+            grandchild->set_type(Cube::Type::OCTANT);
+        }
+    }
+
+    EXPECT_EQ(root->children()[1]->neighbor(Cube::NeighborAxis::Y, Cube::NeighborDirection::POSITIVE),
+              root->children()[3]);
+    EXPECT_NE(root->children()[1]->neighbor(Cube::NeighborAxis::Y, Cube::NeighborDirection::POSITIVE),
+              root->children()[0]);
+    EXPECT_EQ(root->children()[1]->children()[2]->neighbor(Cube::NeighborAxis::Y, Cube::NeighborDirection::POSITIVE),
+              root->children()[3]->children()[0]);
+    EXPECT_EQ(root->children()[1]->children()[2]->neighbor(Cube::NeighborAxis::Z, Cube::NeighborDirection::NEGATIVE),
+              root->children()[0]->children()[3]);
+}
+
+} // namespace
