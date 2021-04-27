202a5056df040ffd9e700127ccc16f94020a6922
Iceflower <iceflower@gmx.de>
2021-04-27 22:06:25 +0200

    [cmake] Fix cmake multi config generation

--- cmake/conan_setup.cmake
+++ cmake/conan_setup.cmake
@@ -18,26 +18,49 @@ list(APPEND CMAKE_PREFIX_PATH ${CMAKE_BINARY_DIR})
 
 if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
     message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
-    file(DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/v0.16.1/conan.cmake"
+    file(DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/0140fdbe363abfc1993f579e8d711067ac4742e1/conan.cmake"
         "${CMAKE_BINARY_DIR}/conan.cmake"
-        EXPECTED_HASH SHA256=396e16d0f5eabdc6a14afddbcfff62a54a7ee75c6da23f32f7a31bc85db23484
+        EXPECTED_HASH SHA256=DDF0FAFACF48B5C4912ECCE5701C252532437C40277734CAD5F4A8084470ADBC
         TLS_VERIFY ON)
 endif()
 
 include(conan)
 
 conan_check(VERSION 1.35.2 REQUIRED)
-conan_cmake_autodetect(conan_settings BUILD_TYPE ${CMAKE_BUILD_TYPE})
-conan_cmake_install(
-    PATH_OR_REFERENCE ${PROJECT_SOURCE_DIR}
-    BUILD outdated
-    REMOTE conan-center
-    PROFILE ${INEXOR_CONAN_PROFILE}
-    OPTIONS build_benchmarks=${benchmark_option}
-    OPTIONS build_tests=${tests_option}
-    SETTINGS ${conan_settings}
-    SETTINGS compiler.cppstd=17
-    ${compiler_libcxx}
-)
-include(conanbuildinfo)
+
+get_property(is_multi_config GLOBAL PROPERTY GENERATOR_IS_MULTI_CONFIG)
+# if a specific build type is set ignore the multi config generator
+if(${is_multi_config} AND NOT DEFINED CMAKE_BUILD_TYPE)
+    foreach(TYPE ${CMAKE_CONFIGURATION_TYPES})
+        conan_cmake_autodetect(conan_settings BUILD_TYPE ${TYPE})
+        conan_cmake_install(
+            PATH_OR_REFERENCE ${PROJECT_SOURCE_DIR}
+            BUILD outdated
+            REMOTE conan-center
+            PROFILE ${INEXOR_CONAN_PROFILE}
+            GENERATOR cmake_multi
+            OPTIONS build_benchmarks=${benchmark_option}
+            OPTIONS build_tests=${tests_option}
+            SETTINGS ${conan_settings}
+            SETTINGS compiler.cppstd=17
+            ${compiler_libcxx}
+        )
+    endforeach()
+    include(conanbuildinfo_multi)
+else()
+    conan_cmake_autodetect(conan_settings)
+    conan_cmake_install(
+        PATH_OR_REFERENCE ${PROJECT_SOURCE_DIR}
+        BUILD outdated
+        REMOTE conan-center
+        PROFILE ${INEXOR_CONAN_PROFILE}
+        OPTIONS build_benchmarks=${benchmark_option}
+        OPTIONS build_tests=${tests_option}
+        SETTINGS ${conan_settings}
+        SETTINGS compiler.cppstd=17
+        ${compiler_libcxx}
+    )
+    include(conanbuildinfo)
+endif()
+
 conan_basic_setup()
