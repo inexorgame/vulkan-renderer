cf75177075184aab0cf13a274429f040d6301af0
Johannes Schneider <johannesschneider@outlook.com>
2021-05-02 22:22:27 +0200

    [tests|benchmarks] Print engine metadata in tests and benchmarks

--- benchmarks/engine_benchmark_main.cpp
+++ benchmarks/engine_benchmark_main.cpp
@@ -1,13 +1,23 @@
 #include <benchmark/benchmark.h>
 
+#include "inexor/vulkan-renderer/meta.hpp"
+
 #include <iostream>
 
 int main(int argc, char **argv) {
+    using namespace inexor::vulkan_renderer;
+
+    // Print engine and application metadata.
+    std::cout << ENGINE_NAME << ", version " << ENGINE_VERSION_STR << std::endl;
+    std::cout << APP_NAME << ", version " << APP_VERSION_STR << std::endl;
+    std::cout << "Configuration: " << BUILD_TYPE << ", Git SHA " << BUILD_GIT << std::endl;
+
     benchmark::Initialize(&argc, argv);
     if (benchmark::ReportUnrecognizedArguments(argc, argv)) {
         return 1;
     }
     benchmark::RunSpecifiedBenchmarks();
 
+    std::cout << "Press Enter to close" << std::endl;
     std::cin.get();
 }
