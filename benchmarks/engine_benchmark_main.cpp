9ac90a1acd74d93e328e9c341063c63473efb125
Johannes Schneider <johannesschneider@outlook.com>
2021-05-02 22:22:27 +0200

    [tests|benchmarks] Print engine metadata in tests and benchmarks

--- benchmarks/engine_benchmark_main.cpp
+++ benchmarks/engine_benchmark_main.cpp
@@ -1,13 +1,23 @@
 #include <benchmark/benchmark.h>
 
+#include "inexor/vulkan-renderer/meta.hpp"
+
 #include <iostream>
 
 int main(int argc, char **argv) {
+    using namespace inexor::vulkan_renderer;
+
+    // Print engine and application metadata.
+    std::cout << m_engine_name << ", version " << m_engine_version_str << std::endl;
+    std::cout << m_application_name << ", version " << m_application_version_str << std::endl;
+    std::cout << "Configuration: " << m_build_type << ", Git SHA " << m_build_git << std::endl;
+
     benchmark::Initialize(&argc, argv);
     if (benchmark::ReportUnrecognizedArguments(argc, argv)) {
         return 1;
     }
     benchmark::RunSpecifiedBenchmarks();
 
+    std::cout << "Press Enter to close" << std::endl;
     std::cin.get();
 }
--- tests/unit_tests_main.cpp
+++ tests/unit_tests_main.cpp
@@ -1,9 +1,20 @@
 #include <gtest/gtest.h>
 
+#include "inexor/vulkan-renderer/meta.hpp"
+
 #include <iostream>
 
 int main(int argc, char **argv) {
+    using namespace inexor::vulkan_renderer;
+
+    // Print engine and application metadata.
+    std::cout << m_engine_name << ", version " << m_engine_version_str << std::endl;
+    std::cout << m_application_name << ", version " << m_application_version_str << std::endl;
+    std::cout << "Configuration: " << m_build_type << ", Git SHA " << m_build_git << std::endl;
+    
     testing::InitGoogleTest(&argc, argv);
     RUN_ALL_TESTS();
+
+    std::cout << "Press Enter to close" << std::endl;
     std::cin.get();
 }
