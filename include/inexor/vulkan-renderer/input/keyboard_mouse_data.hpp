21e49af98bf9802536db63b81903f40979efa14e
Johannes Schneider <johannesschneider@outlook.com>
2021-07-21 21:38:53 +0200

    [input] Ignore invalid key ids and button ids

--- include/inexor/vulkan-renderer/input/keyboard_mouse_data.hpp
+++ include/inexor/vulkan-renderer/input/keyboard_mouse_data.hpp
@@ -28,45 +28,45 @@ public:
     KeyboardMouseInputData &operator=(KeyboardMouseInputData &&) = delete;
 
     /// @brief Change the key's state to pressed.
-    /// @param key the key which was pressed
+    /// @param key the key which was pressed and greater or equal to 0
     void press_key(std::int32_t key);
 
     /// @brief Change the key's state to unpressed.
     /// @param key the key which was released
-    /// @note key must be smaller than ``GLFW_KEY_LAST``
+    /// @note key must be smaller than ``GLFW_KEY_LAST`` and greater or equal to 0
     void release_key(std::int32_t key);
 
     /// @brief Check if the given key is currently pressed.
     /// @param key the key index
-    /// @note key must be smaller than ``GLFW_KEY_LAST``
+    /// @note key must be smaller than ``GLFW_KEY_LAST`` and greater or equal to 0
     /// @return ``true`` if the key is pressed
     [[nodiscard]] bool is_key_pressed(std::int32_t key) const;
 
     /// @brief Checks if a key was pressed once.
     /// @param key The key index
-    /// @note key must be smaller than ``GLFW_KEY_LAST``
+    /// @note key must be smaller than ``GLFW_KEY_LAST`` and greater or equal to 0
     /// @return ``true`` if the key was pressed
     [[nodiscard]] bool was_key_pressed_once(std::int32_t key);
 
     /// @brief Change the mouse button's state to pressed.
     /// @param button the mouse button which was pressed
-    /// @note button must be smaller than ``GLFW_MOUSE_BUTTON_LAST``
+    /// @note button must be smaller than ``GLFW_MOUSE_BUTTON_LAST`` and greater or equal to 0
     void press_mouse_button(std::int32_t button);
 
     /// @brief Change the mouse button's state to unpressed.
     /// @param button the mouse button which was released
-    /// @note button must be smaller than ``GLFW_MOUSE_BUTTON_LAST``
+    /// @note button must be smaller than ``GLFW_MOUSE_BUTTON_LAST`` and greater or equal to 0
     void release_mouse_button(std::int32_t button);
 
     /// @brief Check if the given mouse button is currently pressed.
     /// @param button the mouse button index
-    /// @note button must be smaller than ``GLFW_MOUSE_BUTTON_LAST``
+    /// @note button must be smaller than ``GLFW_MOUSE_BUTTON_LAST`` and greater or equal to 0
     /// @return ``true`` if the mouse button is pressed
     [[nodiscard]] bool is_mouse_button_pressed(std::int32_t button) const;
 
     /// @brief Checks if a mouse button was pressed once.
     /// @param button the mouse button index
-    /// @note button must be smaller than ``GLFW_MOUSE_BUTTON_LAST``
+    /// @note button must be smaller than ``GLFW_MOUSE_BUTTON_LAST`` and greater or equal to 0
     /// @return ``true`` if the mouse button was pressed
     [[nodiscard]] bool was_mouse_button_pressed_once(std::int32_t button);
 
