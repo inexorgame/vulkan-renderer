8aef142018348b761104e3b1e20bb46da71c4c99
Johannes Schneider <johannesschneider@outlook.com>
2021-11-28 10:42:04 +0100

    [file|shader] Refactor file loading code

--- src/vulkan-renderer/wrapper/shader.cpp
+++ src/vulkan-renderer/wrapper/shader.cpp
@@ -1,42 +1,21 @@
 #include "inexor/vulkan-renderer/wrapper/shader.hpp"
 
 #include "inexor/vulkan-renderer/exception.hpp"
+#include "inexor/vulkan-renderer/tools/file.hpp"
 #include "inexor/vulkan-renderer/wrapper/device.hpp"
 #include "inexor/vulkan-renderer/wrapper/make_info.hpp"
 
 #include <spdlog/spdlog.h>
 
 #include <cassert>
-#include <fstream>
 #include <stdexcept>
 #include <utility>
-
-namespace {
-
-std::vector<char> read_binary(const std::string &file_name) {
-    // Open stream at the end of the file to read it's size.
-    std::ifstream file(file_name.c_str(), std::ios::ate | std::ios::binary | std::ios::in);
-
-    if (!file) {
-        throw std::runtime_error("Error: Could not open file " + file_name + "!");
-    }
-
-    const auto file_size = file.tellg();
-    std::vector<char> buffer(file_size);
-
-    file.seekg(0);
-    file.read(buffer.data(), file_size);
-
-    return buffer;
-}
-
-} // namespace
 
 namespace inexor::vulkan_renderer::wrapper {
 
 Shader::Shader(const Device &device, const VkShaderStageFlagBits type, const std::string &name,
                const std::string &file_name, const std::string &entry_point)
-    : Shader(device, type, name, read_binary(file_name), entry_point) {}
+    : Shader(device, type, name, tools::read_file_binary_data(file_name), entry_point) {}
 
 Shader::Shader(const Device &device, const VkShaderStageFlagBits type, const std::string &name,
                const std::vector<char> &code, const std::string &entry_point)
