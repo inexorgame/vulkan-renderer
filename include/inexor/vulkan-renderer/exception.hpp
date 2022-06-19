68b14beef7be1a1c1f25da3faeb73b033a5bbcc2
Johannes Schneider <johannesschneider@outlook.com>
2022-06-19 13:52:26 +0200

    [exception] Use source location feature

--- include/inexor/vulkan-renderer/exception.hpp
+++ include/inexor/vulkan-renderer/exception.hpp
@@ -2,24 +2,29 @@
 
 #include <vulkan/vulkan_core.h>
 
+#include <source_location>
 #include <stdexcept>
 #include <string>
 
 namespace inexor::vulkan_renderer {
 
-/// @brief A custom base class for exceptions
+/// A custom base class for exceptions
 class InexorException : public std::runtime_error {
 public:
-    // No need to define own constructors.
+    // There is no need to define our own constructors
     using std::runtime_error::runtime_error;
 };
 
-/// @brief InexorException for Vulkan specific things.
+/// Custom exception class for Vulkan related exceptions
 class VulkanException final : public InexorException {
 public:
-    /// @param message The exception message.
-    /// @param result The VkResult value of the Vulkan API call which failed.
-    VulkanException(std::string message, VkResult result);
+    /// Default constructor
+    /// Here we are using `C++20 source location feature <https://en.cppreference.com/w/cpp/utility/source_location>`__
+    /// @param message The exception message
+    /// @param result The VkResult value of the Vulkan API call which failed
+    /// @param location The source location
+    VulkanException(std::string message, VkResult result,
+                    std::source_location location = std::source_location::current());
 };
 
 } // namespace inexor::vulkan_renderer
--- src/vulkan-renderer/exception.cpp
+++ src/vulkan-renderer/exception.cpp
@@ -4,11 +4,20 @@
 
 namespace inexor::vulkan_renderer {
 
-VulkanException::VulkanException(std::string message, const VkResult result)
+VulkanException::VulkanException(std::string message, const VkResult result, const std::source_location location)
     : InexorException(message.append(" (")
                           .append(vk_tools::as_string(result))
                           .append(": ")
                           .append(vk_tools::result_to_description(result))
+                          .append(") (")
+                          .append("file: ")
+                          .append(location.file_name())
+                          .append(", line: ")
+                          .append(std::to_string(location.line()))
+                          .append(", column: ")
+                          .append(std::to_string(location.column()))
+                          .append(", function name: ")
+                          .append(location.function_name())
                           .append(")")) {}
 
 } // namespace inexor::vulkan_renderer
