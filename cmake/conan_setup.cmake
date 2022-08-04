f26363244aa3281ba868c8a447c0bdd2f1cea327
Iceflower <iceflower@gmx.de>
2022-08-04 12:39:42 +0200

    [dep] Update dependencies

--- cmake/conan_setup.cmake
+++ cmake/conan_setup.cmake
@@ -18,18 +18,17 @@ list(APPEND CMAKE_PREFIX_PATH ${CMAKE_BINARY_DIR})
 
 if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
     message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
-    file(DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/1ed947da9c0207b070c7967af1c60da636039c33/conan.cmake"
+    file(DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/4b01ffd8c058f5784ebf39b9bebab3961be52888/conan.cmake"
         "${CMAKE_BINARY_DIR}/conan.cmake"
-        EXPECTED_HASH SHA256=8e1ae613d112105fcb43f2837fe81d6d08b4619237a8035731afebbea9646e32
+        EXPECTED_HASH SHA256=B48262E9B603378E16E1056E786BF389EB7F2FE2267DACBB41C5A9E33A07E0F1
         TLS_VERIFY ON)
 endif()
 
 include(conan)
 
-conan_check(VERSION 1.35.2 REQUIRED)
+conan_check(VERSION 1.51.0 REQUIRED)
 
-# TODO: cmake-conan v0.17.0 can automatically set the compiler.cppstd propety based on CMAKE_CXX_STANDARD.
-#       We could set this variable globally but it goes against current conventions where the CXX standard is set per target.
+# We could set this variable globally but it goes against current conventions where the CXX standard is set per target.
 get_property(is_multi_config GLOBAL PROPERTY GENERATOR_IS_MULTI_CONFIG)
 # if a specific build type is set ignore the multi config generator
 if(${is_multi_config} AND NOT DEFINED CMAKE_BUILD_TYPE)
@@ -44,7 +43,6 @@ if(${is_multi_config} AND NOT DEFINED CMAKE_BUILD_TYPE)
             OPTIONS build_benchmarks=${benchmark_option}
             OPTIONS build_tests=${tests_option}
             SETTINGS ${conan_settings}
-            SETTINGS compiler.cppstd=20
             ${compiler_libcxx}
         )
     endforeach()
@@ -59,7 +57,6 @@ else()
         OPTIONS build_benchmarks=${benchmark_option}
         OPTIONS build_tests=${tests_option}
         SETTINGS ${conan_settings}
-        SETTINGS compiler.cppstd=20
         ${compiler_libcxx}
     )
     include(conanbuildinfo)
