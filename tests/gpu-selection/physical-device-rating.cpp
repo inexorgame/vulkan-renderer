1cb1190b2d327c55fc520fdaafb51a96d1ba569d
Johannes Schneider <johannesschneider@outlook.com>
2022-09-10 15:04:20 +0200

    [tests] Add tests for rating of misc physical device types

--- tests/gpu-selection/physical-device-rating.cpp
+++ tests/gpu-selection/physical-device-rating.cpp
@@ -14,4 +14,12 @@ TEST(PhysicalDeviceRating, DisqualifyNoPresentationSupport) {
     EXPECT_EQ(rate_physical_device(VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU, {}, false, true), -1);
 }
 
+TEST(PhysicalDeviceRating, RateMiscPhysicalDeviceTypes) {
+    // Without specifying any memory properties, physical devices which are not discrete gpus or integrated gpus should
+    // get a score of 1 only
+    EXPECT_EQ(rate_physical_device(VK_PHYSICAL_DEVICE_TYPE_OTHER, {}, true, true), 1);
+    EXPECT_EQ(rate_physical_device(VK_PHYSICAL_DEVICE_TYPE_VIRTUAL_GPU, {}, true, true), 1);
+    EXPECT_EQ(rate_physical_device(VK_PHYSICAL_DEVICE_TYPE_CPU, {}, true, true), 1);
+}
+
 } // namespace inexor::vulkan_renderer::wrapper
