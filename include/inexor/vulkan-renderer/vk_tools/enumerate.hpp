983f867bdaad6fa8f996a3ae8119da3b8cf750b2
Johannes Schneider <johannesschneider@outlook.com>
2022-10-14 19:22:43 +0200

    [enumerate] Add new methods

--- include/inexor/vulkan-renderer/vk_tools/enumerate.hpp
+++ include/inexor/vulkan-renderer/vk_tools/enumerate.hpp
@@ -6,6 +6,9 @@
 
 namespace inexor::vulkan_renderer::vk_tools {
 
+/// All functions which contain the word "all" in it, call some vkEnumerate.. function,
+/// while all functions without it call vkGet..
+
 /// Call vkEnumerateDeviceExtensionProperties
 /// @note Because device layers are deprecated in Vulkan, we are not exposing the ``pLayerName`` parameter of
 /// ``vkEnumerateDeviceExtensionProperties`` as a parameter here
@@ -25,5 +28,28 @@ namespace inexor::vulkan_renderer::vk_tools {
 /// @exception VulkanException vkGetPhysicalDeviceQueueFamilyProperties call failed
 /// @return A std::vector of all queue families which are available on the system (this can be empty!)
 [[nodiscard]] std::vector<VkQueueFamilyProperties> get_queue_family_properties(VkPhysicalDevice physical_device);
+
+/// Call vkGetPhysicalDeviceSurfacePresentModesKHR
+/// @param physical_device The physical device
+/// @param surface The surface
+/// @exception VulkanException vkGetPhysicalDeviceSurfaceFormatsKHR call failed
+/// @return A std::vector of surface formats
+[[nodiscard]] std::vector<VkSurfaceFormatKHR> get_surface_formats(VkPhysicalDevice physical_device,
+                                                                  VkSurfaceKHR surface);
+
+/// Call vkGetPhysicalDeviceSurfacePresentModesKHR
+/// @param physical_device The physical device
+/// @param surface The surface
+/// @exception VulkanException vkGetPhysicalDeviceSurfacePresentModesKHR call failed
+/// @return A std::vector of present modes
+[[nodiscard]] std::vector<VkPresentModeKHR> get_surface_present_modes(VkPhysicalDevice physical_device,
+                                                                      VkSurfaceKHR surface);
+
+/// Call vkGetSwapchainImagesKHR
+/// @param device The device
+/// @param swapchain The swapchain to get the images from
+/// @exception inexor::vulkan_renderer::VulkanException vkGetSwapchainImagesKHR call failed
+/// @return A std::vector of swapchain images (this can be empty!)
+[[nodiscard]] std::vector<VkImage> get_swapchain_images(VkDevice device, VkSwapchainKHR swapchain);
 
 } // namespace inexor::vulkan_renderer::vk_tools
--- src/vulkan-renderer/vk_tools/enumerate.cpp
+++ src/vulkan-renderer/vk_tools/enumerate.cpp
@@ -9,16 +9,11 @@ namespace inexor::vulkan_renderer::vk_tools {
 
 std::vector<VkExtensionProperties> get_extension_properties(const VkPhysicalDevice physical_device) {
     assert(physical_device);
-
-    // Query how many device extensions are available for the physical device
     std::uint32_t count = 0;
-    // Because device layers are deprecated, we will never fill the pLayerName parameter
     if (const auto result = vkEnumerateDeviceExtensionProperties(physical_device, nullptr, &count, nullptr);
         result != VK_SUCCESS) {
         throw VulkanException("Error: vkEnumerateDeviceExtensionProperties failed!", result);
     }
-
-    /// Query device extension properties of the physical device
     std::vector<VkExtensionProperties> extensions(count);
     if (const auto result = vkEnumerateDeviceExtensionProperties(physical_device, nullptr, &count, extensions.data());
         result != VK_SUCCESS) {
@@ -29,14 +24,10 @@ std::vector<VkExtensionProperties> get_extension_properties(const VkPhysicalDevi
 
 std::vector<VkPhysicalDevice> get_physical_devices(const VkInstance inst) {
     assert(inst);
-
-    // Query how many physical devices are available on the system
     std::uint32_t count = 0;
     if (const auto result = vkEnumeratePhysicalDevices(inst, &count, nullptr); result != VK_SUCCESS) {
         throw VulkanException("Error: vkEnumeratePhysicalDevices failed!", result);
     }
-
-    // Query physical devices of the system
     std::vector<VkPhysicalDevice> physical_devices(count);
     if (const auto result = vkEnumeratePhysicalDevices(inst, &count, physical_devices.data()); result != VK_SUCCESS) {
         throw VulkanException("Error: vkEnumeratePhysicalDevices failed!", result);
@@ -46,16 +37,59 @@ std::vector<VkPhysicalDevice> get_physical_devices(const VkInstance inst) {
 
 std::vector<VkQueueFamilyProperties> get_queue_family_properties(const VkPhysicalDevice physical_device) {
     assert(physical_device);
-
-    // Query how many queue family properties are available
-    // Because vkGetPhysicalDeviceQueueFamilyProperties has void return type, no error handling is required
     std::uint32_t count = 0;
     vkGetPhysicalDeviceQueueFamilyProperties(physical_device, &count, nullptr);
 
-    /// Query queue family properties
     std::vector<VkQueueFamilyProperties> queue_families(count);
     vkGetPhysicalDeviceQueueFamilyProperties(physical_device, &count, queue_families.data());
     return queue_families;
+}
+
+std::vector<VkSurfaceFormatKHR> get_surface_formats(const VkPhysicalDevice physical_device,
+                                                    const VkSurfaceKHR surface) {
+    assert(physical_device);
+    std::uint32_t count = 0;
+    if (const auto result = vkGetPhysicalDeviceSurfaceFormatsKHR(physical_device, surface, &count, nullptr);
+        result != VK_SUCCESS) {
+        throw VulkanException("Error: vkGetPhysicalDeviceSurfaceFormatsKHR failed!", result);
+    }
+    std::vector<VkSurfaceFormatKHR> surface_formats(count);
+    if (const auto result =
+            vkGetPhysicalDeviceSurfaceFormatsKHR(physical_device, surface, &count, surface_formats.data());
+        result != VK_SUCCESS) {
+        throw VulkanException("Error: vkGetPhysicalDeviceSurfaceFormatsKHR failed!", result);
+    }
+    return surface_formats;
+}
+
+std::vector<VkPresentModeKHR> get_surface_present_modes(const VkPhysicalDevice physical_device,
+                                                        const VkSurfaceKHR surface) {
+    assert(physical_device);
+    std::uint32_t count = 0;
+    if (const auto result = vkGetPhysicalDeviceSurfacePresentModesKHR(physical_device, surface, &count, nullptr);
+        result != VK_SUCCESS) {
+        throw VulkanException("Error: vkGetPhysicalDeviceSurfacePresentModesKHR failed!", result);
+    }
+    std::vector<VkPresentModeKHR> present_modes(count);
+    if (const auto result =
+            vkGetPhysicalDeviceSurfacePresentModesKHR(physical_device, surface, &count, present_modes.data());
+        result != VK_SUCCESS) {
+        throw VulkanException("Error: vkGetPhysicalDeviceSurfacePresentModesKHR failed!", result);
+    }
+    return present_modes;
+}
+
+std::vector<VkImage> get_swapchain_images(const VkDevice device, const VkSwapchainKHR swapchain) {
+    assert(device);
+    std::uint32_t count = 0;
+    if (const auto result = vkGetSwapchainImagesKHR(device, swapchain, &count, nullptr); result != VK_SUCCESS) {
+        throw VulkanException("Error: vkGetSwapchainImagesKHR failed!", result);
+    }
+    std::vector<VkImage> imgs(count);
+    if (const auto result = vkGetSwapchainImagesKHR(device, swapchain, &count, imgs.data()); result != VK_SUCCESS) {
+        throw VulkanException("Error: vkGetSwapchainImagesKHR failed!", result);
+    }
+    return imgs;
 }
 
 } // namespace inexor::vulkan_renderer::vk_tools
