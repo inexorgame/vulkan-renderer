682890b6048eb86e1487285e2deab7612cfbf51e
iceflower <iceflower@gmx.de>
2023-03-20 16:24:05 +0100

    [squash] More conan changes

--- conanfile.py
+++ conanfile.py
@@ -1,60 +1,57 @@
-from conans import ConanFile, CMake
+import os
+
+from conan import ConanFile
+from conan.tools.files import copy
 
 
-class InexorConan(ConanFile):
-    settings = (
-        "os",
-        "compiler",
-        "build_type",
-        "arch"
-    )
+class KenophobiaConan(ConanFile):
+    name = "Kenophobia"
+    version = "0.1.0"
 
-    requires = (
-        "glfw/3.3.8",
-        "glm/cci.20220420",
-        "glslang/1.3.236.0",
-        "imgui/1.89.2",
-        "spdlog/1.11.0",
-        "stb/cci.20220909",
-        "tinygltf/2.5.0",
-        "toml11/3.7.1",
-        "vulkan-headers/1.3.236.0",
-        "vulkan-loader/1.3.236.0",
-        "vulkan-memory-allocator/3.0.1",
-    )
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeToolchain", "CMakeDeps"
 
     options = {
         "build_benchmarks": [True, False],
         "build_tests": [True, False],
+        "use_conan_build_tools": [True, False],
     }
     default_options = {
         "build_benchmarks": False,
         "build_tests": False,
+        # let conan download all build tools, like CMake, Ninja
+        "use_conan_build_tools": False,
     }
 
-    generators = "cmake_find_package"
-
-    def configure(self):
-        if self.settings.os == "Linux" and self.settings.compiler.libcxx == "libstdc++":
-            raise Exception("Inexor is not compatible with libstdc++. "
-                            "Please change the 'compiler.libcxx'-setting "
-                            "(e.g. to libstdc++11).")
-
     def requirements(self):
+        self.requires("glfw/3.3.8")
+        self.requires("glm/cci.20220420")
+        self.requires("glslang/1.3.236.0")
+        self.requires("imgui/1.89.2")
+        self.requires("spdlog/1.11.0")
+        self.requires("stb/cci.20220909", override=True)
+        self.requires("tinygltf/2.5.0")
+        self.requires("toml11/3.7.1")
+        self.requires("vulkan-headers/1.3.236.0")
+        self.requires("vulkan-loader/1.3.236.0")
+        self.requires("vulkan-memory-allocator/3.0.1")
+
         if self.options.build_benchmarks:
             self.requires("benchmark/1.7.1")
         if self.options.build_tests:
             self.requires("gtest/1.13.0")
 
-    def imports(self):
-        # Copies all dll files from packages bin folder to my "bin" folder (win)
-        self.copy("*.dll", dst="bin", src="bin")
-        # Copies all dylib files from packages lib folder to my "lib" folder (macosx)
-        self.copy("*.dylib*", dst="lib", src="lib")
-        # Copies all so files from packages lib folder to my "lib" folder (linux)
-        self.copy("*.so*", dst="lib", src="lib")
+    def build_requirements(self):
+        if self.options.use_conan_build_tools:
+            self.tool_requires("cmake/3.25.2")
+            self.tool_requires("ninja/1.11.1")
 
-    def build(self):
-        cmake = CMake(self)
-        cmake.configure()
-        cmake.build()
+    def validate(self):
+        if self.settings.os == "Linux" and self.settings.compiler.libcxx == "libstdc++":
+            raise Exception("Inexor is not compatible with libstdc++. "
+                            "Please change the 'compiler.libcxx' setting "
+                            "(e.g. to libstdc++11).")
+
+    def package(self):
+        copy(self, "*.dll", os.path.join(self.build_folder, "bin"), os.path.join(self.package_folder, "bin"))
+        copy(self, "*.so*", os.path.join(self.build_folder, "lib"), os.path.join(self.package_folder, "lib"))
