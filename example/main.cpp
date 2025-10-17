3eb3238151b2c7ad3233534aa23d9626ccf1b570
Johannes Schneider <IAmNotHanni@users.noreply.github.com>
2025-10-17 22:37:56 +0200

    [allocators] Experiment with pool allocators

--- example/main.cpp
+++ example/main.cpp
@@ -1,8 +1,38 @@
 ﻿#include "inexor/vulkan-renderer/application.hpp"
 
+#include "inexor/vulkan-renderer/tools/allocators/pool_allocator.hpp"
+
 #include <stdexcept>
+#include <iostream>
 
 int main(int argc, char *argv[]) {
+
+    class Person {
+    public:
+        int m_age;
+        std::string m_name;
+
+        Person(int age, std::string name) : m_age(age), m_name(name) {}
+
+        void print() {
+            std::cout << m_name << " " << m_age << std::endl;
+        }
+
+    };
+
+    inexor::vulkan_renderer::tools::allocators::PoolAllocator<Person> persons;
+
+    auto p1 = persons.allocate(18, "Johannes");
+    auto p2 = persons.allocate(17, "Dieter");
+    auto p3 = persons.allocate(143, "Sabine");
+
+    p1->print();
+    p2->print();
+    p3->print();
+
+    persons.deallocate(p1);
+    persons.deallocate(p2);
+
     try {
         using inexor::vulkan_renderer::Application;
         auto renderer = std::make_unique<Application>(argc, argv);
---
+++ include/inexor/vulkan-renderer/tools/allocators/pool_allocator.hpp
@@ -0,0 +1,82 @@
+#pragma once
+
+#include <stdexcept>
+#include <memory>
+
+namespace inexor::vulkan_renderer::tools::allocators {
+
+
+	template <typename T>
+	union PoolChunk {
+		T value;
+        PoolChunk<T> *next_pool_chunk = nullptr;		
+
+		// TODO: Should we =default this here?
+		PoolChunk() {};
+        ~PoolChunk() {};
+	};
+
+	template <typename T>
+	class PoolAllocator {
+        private:
+            static constexpr std::size_t DEFAULT_SIZE{1024};
+            std::size_t m_size;
+
+			PoolChunk<T> *m_data = nullptr;
+            PoolChunk<T>* m_head = nullptr;
+
+        public:
+            explicit PoolAllocator(const std::size_t size = DEFAULT_SIZE) : m_size(size) {
+                if (m_size == 0) {
+                    throw std::invalid_argument("Error: size is 0!");
+				}
+
+				// This allocates memory, but does not call the constructor of T!
+                m_data = new PoolChunk<T>[size];
+                m_head = m_data;
+
+				for (std::size_t index = 0; index < m_size; index++) {
+					// We must use std::addressof because the & operator could be overloaded!
+                    m_data[index].next_pool_chunk = std::addressof(m_data[index+1]);
+				}
+				m_data[size-1].next_pool_chunk = nullptr;
+			}
+
+			PoolAllocator(const PoolAllocator &other) = delete;
+            PoolAllocator(PoolAllocator &&other) = delete;
+
+			PoolAllocator &operator=(const PoolAllocator &other) = delete;
+            PoolAllocator &operator=(PoolAllocator &&other) = delete;
+
+			~PoolAllocator() {
+                delete[] m_data;
+                m_data = nullptr;
+                m_head = nullptr;
+			}
+
+			template <typename ... arguments>
+			[[nodiscard]] T *allocate(arguments && ... args) {
+                if (m_head==nullptr) {
+                    return nullptr;
+				}
+
+				// TODO: We could simplify this further
+				auto pool_chunk = m_head;
+                m_head = m_head->next_pool_chunk;
+
+				// Placement new
+                return new (std::addressof(pool_chunk->value)) T(std::forward<arguments>(args)...);
+			}
+
+			void deallocate(T* data)
+			{
+				// TODO: Error check for data! Was this really the same type T?
+                data->~T();
+                auto pool_chunk = reinterpret_cast<PoolChunk<T>*>(data);
+                pool_chunk->next_pool_chunk = m_head;
+                m_head = pool_chunk;
+			}
+
+	};
+
+}
