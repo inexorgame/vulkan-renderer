914a36f217769bb5003c8d2ec96fb812e4477acd
Johannes Schneider <IAmNotHanni@users.noreply.github.com>
2025-10-18 17:16:07 +0200

    [allocators] Improve pool allocator

--- example/main.cpp
+++ example/main.cpp
@@ -20,23 +20,24 @@ int main(int argc, char *argv[]) {
 
     };
 
-    inexor::vulkan_renderer::tools::allocators::PoolAllocator<Person> persons;
-
-    auto p1 = persons.allocate(18, "Johannes");
-    auto p2 = persons.allocate(17, "Dieter");
-    auto p3 = persons.allocate(143, "Sabine");
-
-    p1->print();
-    p2->print();
-    p3->print();
-
-    persons.deallocate(p1);
-    persons.deallocate(p2);
-    persons.deallocate(p3);
-
     try {
         using inexor::vulkan_renderer::Application;
         auto renderer = std::make_unique<Application>(argc, argv);
+
+        inexor::vulkan_renderer::tools::allocators::PoolAllocator<Person> persons(1024);
+
+        auto p1 = persons.allocate(18, "Johannes");
+        auto p2 = persons.allocate(17, "Dieter");
+        auto p3 = persons.allocate(143, "Sabine");
+
+        p1->print();
+        p2->print();
+        p3->print();
+
+        persons.deallocate(p1);
+        persons.deallocate(p2);
+        persons.deallocate(p3);
+
         renderer->run();
     } catch (const std::exception &exception) {
         spdlog::critical(exception.what());
--- include/inexor/vulkan-renderer/tools/allocators/pool_allocator.hpp
+++ include/inexor/vulkan-renderer/tools/allocators/pool_allocator.hpp
@@ -2,6 +2,10 @@
 
 #include <stdexcept>
 #include <memory>
+#include <cassert>
+#include <mutex>
+#include <shared_mutex>
+#include <utility>
 
 namespace inexor::vulkan_renderer::tools::allocators {
 
@@ -19,25 +23,32 @@ namespace inexor::vulkan_renderer::tools::allocators {
 	class PoolAllocator {
         private:
             static constexpr std::size_t DEFAULT_SIZE{1024};
-            std::size_t m_size;
+            std::size_t m_size = 0;
+            std::size_t m_alloc_in_use = 0;
 
 			PoolChunk<T> *m_data = nullptr;
             PoolChunk<T>* m_head = nullptr;
 
+            std::shared_mutex m_mutex;
+
         public:
             explicit PoolAllocator(const std::size_t size = DEFAULT_SIZE) : m_size(size) {
+                // Note that object construction is not concurrent: We assume that this is externally synchronized,
+                // because we can't use an object until its constructor has finished anyways.
+                // This means we do not need a lock guard here!
                 if (m_size == 0) {
-                    throw std::invalid_argument("Error: size is 0!");
+                    throw std::invalid_argument("Error: pool size is 0!");
 				}
-
 				// This allocates memory, but does not call the constructor of T!
                 m_data = new PoolChunk<T>[size];
+                // Point the head to the first index of the array.
                 m_head = m_data;
-
+                // Fill single linked list with data to the next pool chunk
 				for (std::size_t index = 0; index < m_size-1; index++) {
 					// We must use std::addressof because the & operator could be overloaded!
                     m_data[index].next_pool_chunk = std::addressof(m_data[index+1]);
 				}
+                // In the last pool chunk, set the data to the next pool chunk to nullptr.
 				m_data[size-1].next_pool_chunk = nullptr;
 			}
 
@@ -48,6 +59,10 @@ namespace inexor::vulkan_renderer::tools::allocators {
             PoolAllocator &operator=(PoolAllocator &&other) = delete;
 
 			~PoolAllocator() {
+                std::unique_lock lock(m_mutex);
+                // If this is not 0, we have allocations that are still in use and whose destructors have NOT been called yet!
+                // Note that we can't throw an exception in a destructor, because std::terminate will be invoked!
+                assert(m_alloc_in_use == 0 && "Error: Not all allocations in the pool allocator have been freed!");
                 delete[] m_data;
                 m_data = nullptr;
                 m_head = nullptr;
@@ -55,22 +70,35 @@ namespace inexor::vulkan_renderer::tools::allocators {
 
 			template <typename ... arguments>
 			[[nodiscard]] T *allocate(arguments && ... args) {
+                std::unique_lock lock(m_mutex);
                 if (m_head==nullptr) {
-                    return nullptr;
+                    throw std::runtime_error("Error: Out of memory!");
 				}
+                m_alloc_in_use++;
                 auto *chunk = std::exchange(m_head, m_head->next_pool_chunk);
-                // Placement new
+                // Create the new object by using placement new and perfect forwarding.
                 return new (std::addressof(chunk->value)) T(std::forward<arguments>(args)...);
             }
 
 			void deallocate(T* data)
 			{
-				// TODO: Error check for data! Was this really the same type T?
+                // Note that we need both reinterpret_cast here
+                if (data > reinterpret_cast<T*>(m_data+m_size) || data < reinterpret_cast<T*>(m_data)) {
+                    throw std::logic_error("Error: The deallocate() method was called with a pointer that was not allocated from the allocator!");
+                }
+                std::unique_lock lock(m_mutex);
+                // Invoke the destructor of the allocated object manually.
                 data->~T();
-                auto pool_chunk = reinterpret_cast<PoolChunk<T>*>(data);
+                m_alloc_in_use--;
+                auto* pool_chunk = reinterpret_cast<PoolChunk<T>*>(data);
                 pool_chunk->next_pool_chunk = m_head;
                 m_head = pool_chunk;
 			}
+
+            [[nodiscard]] auto size() const noexcept {
+                std::shared_lock lock(m_mutex);
+                return m_size;
+            }
 
 	};
 
