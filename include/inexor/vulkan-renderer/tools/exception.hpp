aa177062dcb3053808f7744dca39cad86fe5f06f
IAmNotHanni <johannesschneider@outlook.com>
2025-10-19 22:28:45 +0200

    [exception] Use std::source_location and allow for custom Vulkan object naming

--- include/inexor/vulkan-renderer/tools/exception.hpp
+++ include/inexor/vulkan-renderer/tools/exception.hpp
@@ -2,24 +2,32 @@
 
 #include <volk.h>
 
+#include <source_location>
 #include <stdexcept>
 #include <string>
 
 namespace inexor::vulkan_renderer::tools {
 
-/// @brief A custom base class for exceptions
+/// A custom base class for exceptions
 class InexorException : public std::runtime_error {
 public:
-    // No need to define own constructors.
-    using std::runtime_error::runtime_error;
+    /// Default constructor
+    /// @param message The exception error message
+    /// @param location The std::source_location which will be turned into useful debug text
+    InexorException(std::string message, std::source_location location = std::source_location::current());
 };
 
-/// @brief InexorException for Vulkan specific things.
+/// InexorException for Vulkan specific things.
 class VulkanException final : public InexorException {
 public:
-    /// @param message The exception message.
-    /// @param result The VkResult value of the Vulkan API call which failed.
-    VulkanException(std::string message, VkResult result);
+    /// Default constructor
+    /// @param message The exception message
+    /// @param result The VkResult value of the Vulkan API call which failed
+    /// @param object_name The name of the Vulkan object which was attempted to be created (empty by default)
+    /// @param location The std::source_location which will be turned into useful debug text
+    /// (``std::source_location::current()`` by default)
+    VulkanException(std::string message, VkResult result, std::string object_name = "",
+                    std::source_location location = std::source_location::current());
 };
 
 } // namespace inexor::vulkan_renderer::tools
--- src/vulkan-renderer/tools/exception.cpp
+++ src/vulkan-renderer/tools/exception.cpp
@@ -4,11 +4,21 @@
 
 namespace inexor::vulkan_renderer::tools {
 
-VulkanException::VulkanException(std::string message, const VkResult result)
-    : InexorException(message.append(" (")
+InexorException::InexorException(std::string message, std::source_location location)
+    : std::runtime_error("\n\tFILE:\t  " + std::string(location.file_name()) +
+                         "\n\tFUNCTION: " + std::string(location.function_name()) + "\n\tLINE:\t  " +
+                         std::to_string(location.line()) + "\n\tERROR:\t  " + std::string(message)) {}
+
+VulkanException::VulkanException(std::string message, const VkResult result, const std::string object_name,
+                                 const std::source_location location)
+    : InexorException(message.append("[")
+                          .append(object_name)
+                          .append("]")
+                          .append(" (")
                           .append(tools::as_string(result))
                           .append(": ")
                           .append(tools::result_to_description(result))
-                          .append(")")) {}
+                          .append(")"),
+                      location) {}
 
 } // namespace inexor::vulkan_renderer::tools
